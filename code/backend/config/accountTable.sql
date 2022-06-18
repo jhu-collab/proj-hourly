37beb1d0704518e574c19d09db12362add4eb589
ChrisWilhelm <chriswilhelm2019@gmail.com>
2022-06-17 21:23:20 -0400

    Added prisma and set up a basic account model

--- code/backend/config/accountTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE account (
-    accountId INT GENERATED ALWAYS as IDENTITY,
-    userName TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    phoneNumber TEXT UNIQUE,
-    PRIMARY KEY(accountId)
-);
