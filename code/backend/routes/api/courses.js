f42d38ae83e4ba01596f248599434dee87e16a06
ChrisWilhelm <chriswilhelm2019@gmail.com>
2022-06-19 20:32:19 -0400

    Added route for creating course with intended middleware for validation of request

---
+++ code/backend/routes/api/courses.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { body } = require('express-validator');
+const validator = require('../../middleware/courseValidator');
+const controller = require('../../controllers/courseController');
+
+const router = express.Router();
+
+router.post(
+  '/',
+  body('title', 'Course title is required').notEmpty(),
+  body('number', 'Course number is required').notEmpty(),
+  body('semester', 'Semester is required').isIn([
+    'Spring',
+    'Summer',
+    'Fall',
+    'Winter',
+  ]),
+  body('year', 'A valid year is required').isInt({
+    min: new Date().getFullYear(),
+  }),
+  validator.isUniqueCourse,
+  controller.create,
+);
