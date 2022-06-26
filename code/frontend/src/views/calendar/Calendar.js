fbc4483d89502db9a8028cb158b4097f02a6185a
Samuel Atefah <satefah1@jhu.edu>
2022-06-26 03:10:10 -0400

    Calendar: added time grid layout

--- code/frontend/src/views/calendar/Calendar.js
+++ code/frontend/src/views/calendar/Calendar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 // material-ui
-import { Paper } from '@mui/material';
+import { Paper, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 // full calendar
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import interactionPlugin from '@fullcalendar/interaction'; // needed for dayClick
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 // project imports
 //import { useStore } from '../../store/appStore';
@@ -18,9 +20,11 @@ import interactionPlugin from '@fullcalendar/interaction'; // needed for dayClic
  * @returns The Home component.
  */
 const Calendar = () => {
+  const theme = useTheme();
+  const matchUpSm = useMediaQuery(theme.breakpoints.up('sm'));
+
   const handleEventClick = (info) => {
     // bind with an arrow function
-    // change the border color just for fun
     alert('Event: ' + info.event.title);
     alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
     alert('View: ' + info.view.type);
@@ -29,20 +33,30 @@ const Calendar = () => {
   return (
     <Paper sx={{ padding: '20px', width: '100%', height: '100%' }}>
       <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        headerToolbar={
+          matchUpSm
+            ? {
+                start: 'dayGridMonth,timeGridWeek,timeGridDay',
+                center: 'title',
+              }
+            : { start: 'title', end: 'prev,next' }
+        }
+        initialView="timeGridWeek"
         events={[
-          { title: "Professor Madooei's Office Hours", date: '2022-06-01' },
-          { title: "Tarik's Office Hours", date: '2022-06-02' },
-          { title: "Sofia's Office Hours", date: '2022-06-03' },
-          { title: "Xinan's Office Hours", date: '2022-06-04' },
-          { title: "Chiamaka's Office Hours", date: '2022-06-05' },
-          { title: "Samuel's Office Hours", date: '2022-06-06' },
-          { title: "Chris's Office Hours", date: '2022-06-07' },
+          { title: "Professor Madooei's Office Hours", date: '2022-06-26' },
+          { title: "Tarik's Office Hours", date: '2022-06-27' },
+          { title: "Sofia's Office Hours", date: '2022-06-28' },
+          { title: "Xinan's Office Hours", date: '2022-06-29' },
+          { title: "Chiamaka's Office Hours", date: '2022-06-30' },
+          { title: "Samuel's Office Hours", date: '2022-07-01' },
+          { title: "Chris's Office Hours", date: '2022-07-02' },
         ]}
         eventClick={handleEventClick}
+        dateClick={handleEventClick}
         height="100%"
         editable
+        selectable
       />
     </Paper>
   );
