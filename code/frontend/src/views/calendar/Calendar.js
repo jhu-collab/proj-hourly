3d39188356137394f58d779b1b33d2ebb8165e3f
Samuel Atefah <satefah1@jhu.edu>
2022-06-26 03:27:46 -0400

    Sample Data: added time and recurrence to sample data

--- code/frontend/src/views/calendar/Calendar.js
+++ code/frontend/src/views/calendar/Calendar.js
@@ -9,6 +9,7 @@ import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import interactionPlugin from '@fullcalendar/interaction'; // needed for dayClick
 import timeGridPlugin from '@fullcalendar/timegrid';
+import eventData from './calendar-data/event-data';
 
 // project imports
 //import { useStore } from '../../store/appStore';
@@ -26,8 +27,6 @@ const Calendar = () => {
   const handleEventClick = (info) => {
     // bind with an arrow function
     alert('Event: ' + info.event.title);
-    alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-    alert('View: ' + info.view.type);
   };
 
   return (
@@ -43,15 +42,7 @@ const Calendar = () => {
             : { start: 'title', end: 'prev,next' }
         }
         initialView="timeGridWeek"
-        events={[
-          { title: "Professor Madooei's Office Hours", date: '2022-06-26' },
-          { title: "Tarik's Office Hours", date: '2022-06-27' },
-          { title: "Sofia's Office Hours", date: '2022-06-28' },
-          { title: "Xinan's Office Hours", date: '2022-06-29' },
-          { title: "Chiamaka's Office Hours", date: '2022-06-30' },
-          { title: "Samuel's Office Hours", date: '2022-07-01' },
-          { title: "Chris's Office Hours", date: '2022-07-02' },
-        ]}
+        events={eventData}
         eventClick={handleEventClick}
         dateClick={handleEventClick}
         height="100%"
---
+++ code/frontend/src/views/calendar/calendar-data/event-data.js
@@ -0,0 +1,45 @@
+// ===========================|| CALENDAR - EVENT DATA ||=========================== //
+
+const eventData = [
+  {
+    title: "Professor Madooei's Office Hours",
+    startRecur: '2022-06-26',
+    startTime: '10:30:00',
+    endTime: '11:30:00',
+    daysOfWeek: ['0', '2', '4'],
+  },
+  {
+    title: "Tarik's Office Hours",
+    start: '2022-06-27T14:00:00',
+    end: '2022-06-27T15:00:00',
+  },
+  {
+    title: "Sofia's Office Hours",
+    startRecur: '2022-06-26',
+    startTime: '12:00:00',
+    endTime: '13:00:00',
+    daysOfWeek: ['2', '4', '6'],
+  },
+  {
+    title: "Xinan's Office Hours",
+    start: '2022-06-29T17:00:00',
+    end: '2022-06-29T18:00:00',
+  },
+  {
+    title: "Chiamaka's Office Hours",
+    start: '2022-06-30T13:30:00',
+    end: '2022-06-30T14:30:00',
+  },
+  {
+    title: "Samuel's Office Hours",
+    start: '2022-07-01T08:00:00',
+    end: '2022-07-01T09:00:00',
+  },
+  {
+    title: "Chris's Office Hours",
+    start: '2022-07-02T11:00:00',
+    end: '2022-07-02T12:00:00',
+  },
+];
+
+export default eventData;
