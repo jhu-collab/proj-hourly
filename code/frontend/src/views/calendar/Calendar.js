99627e21287753dbb3e182452ff3c46d2e230b1b
Samuel Atefah <satefah1@jhu.edu>
2022-06-26 01:08:57 -0400

    Calendar: created calendar with sample events

--- code/frontend/src/views/calendar/Calendar.js
+++ code/frontend/src/views/calendar/Calendar.js
@@ -1,13 +1,15 @@
-// material-ui
-//import { Grid } from '@mui/material';
-
-// project imports
 import React from 'react';
+
+// material-ui
+import { Paper } from '@mui/material';
+
+// full calendar
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import interactionPlugin from '@fullcalendar/interaction'; // needed for dayClick
 
-import { useStore } from '../../store/appStore';
-import { Paper } from '@mui/material';
+// project imports
+//import { useStore } from '../../store/appStore';
 
 // ==============================|| HOME ||============================== //
 
@@ -16,16 +18,31 @@ import { Paper } from '@mui/material';
  * @returns The Home component.
  */
 const Calendar = () => {
-  //const createPopup = useStore((state) => state.createPopup);
-  const courses = useStore((state) => state.courses);
-  console.log(courses);
+  const handleEventClick = (info) => {
+    // bind with an arrow function
+    // change the border color just for fun
+    alert('Event: ' + info.event.title);
+    alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+    alert('View: ' + info.view.type);
+  };
 
   return (
-    <Paper sx={{ padding: '20px', width: '100%' }}>
+    <Paper sx={{ padding: '20px', width: '100%', height: '100%' }}>
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
-        handleWindowResize
+        events={[
+          { title: "Professor Madooei's Office Hours", date: '2022-06-01' },
+          { title: "Tarik's Office Hours", date: '2022-06-02' },
+          { title: "Sofia's Office Hours", date: '2022-06-03' },
+          { title: "Xinan's Office Hours", date: '2022-06-04' },
+          { title: "Chiamaka's Office Hours", date: '2022-06-05' },
+          { title: "Samuel's Office Hours", date: '2022-06-06' },
+          { title: "Chris's Office Hours", date: '2022-06-07' },
+        ]}
+        eventClick={handleEventClick}
+        height="100%"
+        editable
       />
     </Paper>
   );
