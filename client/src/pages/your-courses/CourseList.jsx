166bf9295f86b8467c94404c1c58da8c8ae0dbd3
Samuel Atefah <satefah1@jhu.edu>
2022-07-05 18:41:45 -0400

    Empty data: alert component is displayed when no data is available

---
+++ client/src/pages/your-courses/CourseList.jsx
@@ -0,0 +1,40 @@
+import { Alert, AlertTitle, Grid, useTheme } from "@mui/material";
+import React from "react";
+import useStore from "../../services/store";
+import CourseCard from "./CourseCard";
+import { staffCourses, studentCourses } from "./courses-data";
+
+function CourseList() {
+  const { courseType } = useStore();
+
+  const theme = useTheme();
+
+  const courseList = (courses, type) => {
+    if (courses.length > 0) {
+      return courses.map((course, index) => {
+        return (
+          <Grid item xs={12}>
+            <CourseCard course={course} />
+          </Grid>
+        );
+      });
+    } else {
+      return (
+        <Grid item xs={12}>
+          <Alert severity="info" sx={{ mt: theme.spacing(2) }}>
+            <AlertTitle>
+              You are not enrolled in any courses in which you are a {type}.
+            </AlertTitle>
+          </Alert>
+        </Grid>
+      );
+    }
+  };
+
+  return courseList(
+    courseType === "student" ? studentCourses : staffCourses,
+    courseType === "student" ? "student" : "staff member"
+  );
+}
+
+export default CourseList;
--- client/src/pages/your-courses/index.jsx
+++ client/src/pages/your-courses/index.jsx
@@ -1,44 +1,25 @@
 import {
   Grid,
   Stack,
-  ToggleButton,
-  ToggleButtonGroup,
   Typography,
   useTheme,
 } from "@mui/material";
 import React from "react";
-import MainCard from "../../components/MainCard";
-import useStore from "../../services/store";
-import CourseCard from "./CourseCard";
-import { staffCourses, studentCourses } from "./courses-data";
+import CourseList from "./CourseList";
 import CoursesToggleGroup from "./CoursesToggleGroup";
 
 function YourCourses() {
   const theme = useTheme();
 
-  const { courseType } = useStore();
-
-  const courseList = (courses) => {
-    return courses.map((course, index) => {
-      return (
-        <Grid item xs={12}>
-          <CourseCard course={course} />
-        </Grid>
-      );
-    });
-  };
-
   return (
-    <Grid rowSpacing={4.5} columnSpacing={2.75}>
+    <Grid container>
       <Grid item xs={12}>
         <Stack direction="row" alignItems="center" spacing={theme.spacing(2)}>
           <Typography variant="h4">Your Courses</Typography>
           <CoursesToggleGroup />
         </Stack>
       </Grid>
-      {courseType === "student"
-        ? courseList(studentCourses)
-        : courseList(staffCourses)}
+      <CourseList />
     </Grid>
   );
 }
