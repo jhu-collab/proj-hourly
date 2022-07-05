94198a49b86b0df5da04c8fad9e1c7ea08ca414f
Samuel Atefah <satefah1@jhu.edu>
2022-07-05 18:09:25 -0400

    Toggle: can now toggle between student and staff courses

--- client/src/pages/your-courses/courses-data.js
+++ client/src/pages/your-courses/courses-data.js
@@ -7,15 +7,31 @@ export const studentCourses = [
     calendarYear: 2022,
     code: "111111",
   },
+  {
+    id: 2,
+    title: "User Interfaces & Mobile Applications",
+    courseNumber: "601.290",
+    semester: "Spring",
+    calendarYear: 2023,
+    code: "222222",
+  },
 ];
 
 export const staffCourses = [
   {
-    id: 2,
+    id: 3,
     title: "Object Oriented Software Engineering",
     courseNumber: "600.421",
+    semester: "Fall",
+    calendarYear: 2022,
+    code: "333333",
+  },
+  {
+    id: 4,
+    title: "Data Structures",
+    courseNumber: "600.226",
     semester: "Spring",
     calendarYear: 2023,
-    code: "222222",
+    code: "444444",
   },
 ];
--- client/src/pages/your-courses/index.jsx
+++ client/src/pages/your-courses/index.jsx
@@ -8,24 +8,37 @@ import {
 } from "@mui/material";
 import React from "react";
 import MainCard from "../../components/MainCard";
+import useStore from "../../services/store";
 import CourseCard from "./CourseCard";
-import { studentCourses } from "./courses-data";
+import { staffCourses, studentCourses } from "./courses-data";
 import CoursesToggleGroup from "./CoursesToggleGroup";
 
 function YourCourses() {
   const theme = useTheme();
 
+  const { courseType } = useStore();
+
+  const courseList = (courses) => {
+    return courses.map((course, index) => {
+      return (
+        <Grid item xs={12}>
+          <CourseCard course={course} />
+        </Grid>
+      );
+    });
+  };
+
   return (
     <Grid rowSpacing={4.5} columnSpacing={2.75}>
-      <Grid item xs={12} >
+      <Grid item xs={12}>
         <Stack direction="row" alignItems="center" spacing={theme.spacing(2)}>
           <Typography variant="h4">Your Courses</Typography>
           <CoursesToggleGroup />
         </Stack>
       </Grid>
-      <Grid item xs={12} >
-        <CourseCard course={studentCourses[0]} />
-      </Grid>
+      {courseType === "student"
+        ? courseList(studentCourses)
+        : courseList(staffCourses)}
     </Grid>
   );
 }
