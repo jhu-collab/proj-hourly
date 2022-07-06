07e83f830bf2220d0916db4ad031349489b5c733
Samuel Atefah <satefah1@jhu.edu>
2022-07-06 19:27:30 -0400

    Validation: added validation for create course form

---
+++ client/src/pages/your-courses/create-course/validation.js
@@ -0,0 +1,21 @@
+import * as yup from "yup";
+
+export const createCourseSchema = yup.object().shape({
+  title: yup.string().required("Course title is required"),
+  number: yup
+    .string()
+    .matches(/^\d{3}\..{3}$/, "Course number is invalid. Must be xxx.xxx")
+    .required("Course number is required"),
+  // Need to add validation to ensure semester and year are not before current year
+  semester: yup
+    .string()
+    .oneOf(
+      ["Fall", "Winter", "Spring", "Summer"],
+      "Please enter a valid semester"
+    )
+    .required("Semester is required"),
+  year: yup
+    .string()
+    .matches(/^[0-9]{4}$/, "Please enter a valid year")
+    .required("Year is required"),
+});
