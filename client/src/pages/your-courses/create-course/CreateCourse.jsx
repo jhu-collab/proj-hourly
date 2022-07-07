6fc54ea0210b4f99aa1dbf1fa7a94126eebcca10
Samuel Atefah <satefah1@jhu.edu>
2022-07-06 20:30:01 -0400

    Clean up: deleted unused code

--- client/src/pages/your-courses/create-course/CreateCourse.jsx
+++ client/src/pages/your-courses/create-course/CreateCourse.jsx
@@ -1,9 +1,7 @@
-import { Stack, Typography, useTheme } from "@mui/material";
 import Popup from "../../../components/Popup";
 import CreateCourseForm from "./CreateCourseForm";
 
 function CreateCourse({ open, handlePopupToggle }) {
-  const theme = useTheme();
   return (
     <Popup open={open} onClose={handlePopupToggle} title="Create Course">
         <CreateCourseForm />
