d558bd898044d8de28ed9c21fe5acb206c6c9348
Samuel Atefah <satefah1@jhu.edu>
2022-07-08 08:48:39 -0400

    Calendar: created basic FullCalendar component

--- client/src/pages/calendar/Calendar.jsx
+++ client/src/pages/calendar/Calendar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import "@fullcalendar/react/dist/vdom"; // necessary to work with vite configuration
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 
+
+const events = [
+    {
+      id: 1,
+      title: "Coding",
+      start: new Date(new Date().setHours(new Date().getHours() - 3)),
+      end: new Date(new Date().setHours(new Date().getHours() + 3)),
+    },
+  ];
+
+/**
+ * A component that represents the Calendar page for a course.
+ * @returns The Calendar component.
+ */
 function Calendar() {
   return (
-    <div>Calendar</div>
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+    />
   )
 }
 
