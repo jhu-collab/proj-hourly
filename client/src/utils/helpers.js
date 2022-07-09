d547a0165fc85add85d287393fc49ea6fe0d9e19
Samuel Atefah <satefah1@jhu.edu>
2022-07-08 20:42:35 -0400

    Helpers: added a helper function get a locale time string

---
+++ client/src/utils/helpers.js
@@ -0,0 +1,12 @@
+/**
+ * Converts military time to standard time.
+ * @param {string} time - a string representing a hh:mm:ss military time
+ * @returns a short localized time string.
+ */
+ export function getLocaleTime(time) {
+    const [hour, min] = time.split(':');
+    const timeObj = new Date();
+    timeObj.setHours(hour);
+    timeObj.setMinutes(min);
+    return timeObj.toLocaleTimeString([], { timeStyle: 'short' });
+  }
