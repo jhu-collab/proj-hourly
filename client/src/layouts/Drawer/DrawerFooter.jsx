8267a266a19e844d7c9d07bf397255ffbeb62646
Ali Madooei <alimadooei@gmail.com>
2022-07-09 16:49:26 -0400

    Remove BottomNavigation to resolve the warning about showLabel

--- client/src/layouts/Drawer/DrawerFooter.jsx
+++ client/src/layouts/Drawer/DrawerFooter.jsx
@@ -1,35 +1,29 @@
-import useTheme from "@mui/material/styles/useTheme";
-import BottomNavigation from "@mui/material/BottomNavigation";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Chip from "@mui/material/Chip";
-
 import { useState } from "react";
 
 function DrawerFooter() {
-  const theme = useTheme();
   const [value] = useState(import.meta.env.VITE_RUN_MODE);
 
   return (
-    <BottomNavigation sx={{ padding: theme.spacing(1) }}>
-      <List>
-        {value === "local" && (
-          <ListItem>
-            <Chip label="Dev (Local)" size="small" color="secondary" />
-          </ListItem>
-        )}
-        {value === "dev" && (
-          <ListItem>
-            <Chip label="Dev (Staging)" size="small" />
-          </ListItem>
-        )}
-        {value === "prod" && (
-          <ListItem>
-            <Chip label="PRODUCTION" size="small" variant="outlined" />
-          </ListItem>
-        )}
-      </List>
-    </BottomNavigation>
+    <List>
+      {value === "local" && (
+        <ListItem>
+          <Chip label="Dev (Local)" size="small" color="secondary" />
+        </ListItem>
+      )}
+      {value === "dev" && (
+        <ListItem>
+          <Chip label="Dev (Staging)" size="small" />
+        </ListItem>
+      )}
+      {value === "prod" && (
+        <ListItem>
+          <Chip label="PRODUCTION" size="small" variant="outlined" />
+        </ListItem>
+      )}
+    </List>
   );
 }
 
