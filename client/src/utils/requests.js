170b6176db848391fd5270edaba2d70dbcec5818
Samuel Atefah <satefah1@jhu.edu>
2022-07-09 19:41:07 -0400

    Documentation: added a comment regarding id key in header of http requests

--- client/src/utils/requests.js
+++ client/src/utils/requests.js
@@ -5,7 +5,8 @@ import axios from "axios";
 export const getCourses = () => {
     axios
       .get(`${BASE_URL}/api/account/me/courses`, {
-        headers: { "id": 1 },
+         // TODO: Need to remove once backend implements user tokens
+        headers: { "id": 1 }, 
       })
       .then((res) => res.data)
 };
