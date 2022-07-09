06ea65c9f87fba3dbc86d785cc771fe42594a748
Samuel Atefah <satefah1@jhu.edu>
2022-07-08 20:42:51 -0400

    Validation: created validation schema for create event form

--- client/src/utils/validators.js
+++ client/src/utils/validators.js
@@ -1,5 +1,7 @@
 import * as yup from "yup";
 
+const CURRENT_DATE_STR = new Date().toLocaleDateString();
+
 export const createCourseSchema = yup.object().shape({
   title: yup.string().required("Course title is required"),
   courseNumber: yup
@@ -18,3 +20,15 @@ export const createCourseSchema = yup.object().shape({
     .string().length(4, "Please enter a valid year")
     .required("Year is required"),
 });
+
+export const createEventSchema = yup.object().shape({
+  date: yup.date().typeError("Please enter a valid date").min(CURRENT_DATE_STR, `Date must be on or after ${CURRENT_DATE_STR}`).required("Date is required"),
+  // TODO: Add further validation for the startTime and endTime fields
+  startTime: yup.
+  string().required('Start time is required'),
+  endTime: yup.
+  string().required('End time is required'),
+  location: yup
+    .string().required('Location is required'),
+});
+
