d70bcaf3b8ecdb84d0a2bf3a15e8e979dd612731
ChrisWilhelm <chriswilhelm2019@gmail.com>
2022-07-10 12:23:01 -0400

    Added delete account route

--- server/src/controllers/accountController.js
+++ server/src/controllers/accountController.js
@@ -77,3 +77,81 @@ export const getCourses = async (req, res) => {
     instructor: instructorCourses,
   });
 };
+
+export const deleteAccount = async (req, res) => {
+  validate(req);
+  const id = parseInt(req.get("id"), 10);
+  await prisma.registration.deleteMany({
+    where: {
+      accountId: id,
+    },
+  });
+  const officeHours = await prisma.officeHour.findMany({
+    where: {
+      hosts: {
+        some: {
+          id,
+        },
+      },
+    },
+  });
+  officeHours.forEach(async (officeHour) => {
+    if (officeHour.hosts.length === 1) {
+      await prisma.officeHour.delete({
+        where: {
+          id: officeHour.id,
+        },
+      });
+    } else {
+      await prisma.officeHour.update({
+        where: {
+          id: officeHour,
+        },
+        data: {
+          hosts: {
+            disconnect: {
+              id,
+            },
+          },
+        },
+      });
+    }
+  });
+  const courses = await prisma.course.findMany({
+    where: {
+      instructors: {
+        some: {
+          id,
+        },
+      },
+    },
+  });
+  courses.forEach(async (course) => {
+    if (course.instructors.length === 1) {
+      await prisma.course.delete({
+        where: {
+          id: course.id,
+        },
+      });
+    } else {
+      await prisma.course.update({
+        where: {
+          id: course.id,
+        },
+        data: {
+          instructors: {
+            disconnect: {
+              id,
+            },
+          },
+        },
+      });
+    }
+  });
+  await prisma.account.delete({
+    where: {
+      id,
+    },
+  });
+  return res.status(StatusCodes.ACCEPTED).json({ msg: "Account deleted!" });
+};
