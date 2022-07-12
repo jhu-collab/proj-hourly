5d024af6d008db8cdd32b71219708755246ec8b3
Xinan Rahman <xinanrahman03@gmail.com>
2022-07-12 17:06:56 -0400

    minor tweaks to import statements to increase efficiency

--- client/src/pages/your-courses/join-course/JoinCourseForm.jsx
+++ client/src/pages/your-courses/join-course/JoinCourseForm.jsx
@@ -6,7 +6,8 @@ import { toast } from "react-toastify";
 import Form from "../../../components/form-ui/Form";
 import FormInputText from "../../../components/form-ui/FormInputText";
 import { joinCourseSchema } from "../../../utils/validators";
-import { useMutation, useQueryClient } from "react-query";
+import { useMutation } from "react-query";
+import { useQueryClient } from "react-query";
 import { joinCourse } from "../../../utils/requests";
 
 /**
