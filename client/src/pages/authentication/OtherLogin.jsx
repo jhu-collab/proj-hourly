e5616ed702730f93753df6cf56b9bf373cfd9611
Samuel Atefah <satefah1@jhu.edu>
2022-07-14 03:21:55 -0400

    OtherLogin: animated the button

--- client/src/pages/authentication/OtherLogin.jsx
+++ client/src/pages/authentication/OtherLogin.jsx
@@ -1,7 +1,9 @@
 import Button from "@mui/material/Button";
+import AnimateButton from "../../components/AnimateButton";
 
 function OtherLogin() {
   return (
+    <AnimateButton>
     <Button
       variant="outlined"
       color="primary"
@@ -10,6 +12,7 @@ function OtherLogin() {
     >
       JHU SSO
     </Button>
+    </AnimateButton>
   );
 }
 
