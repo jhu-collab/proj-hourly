5bfaece676d159486d134b250cf87ae2fa43ca9f
Samuel Atefah <satefah1@jhu.edu>
2022-07-14 21:59:19 -0400

    Event Details: made some progress on the popover

--- client/src/pages/calendar/event-details/EventDetails.jsx
+++ client/src/pages/calendar/event-details/EventDetails.jsx
@@ -1,13 +1,16 @@
-import { Grid, Popover, Stack, Typography } from "@mui/material";
+import CloseOutlined from "@ant-design/icons/CloseOutlined";
+import { Grid, IconButton, Popover, Stack, Typography } from "@mui/material";
 import React from "react";
 
 function EventDetails({ anchorEl, handleClose, event }) {
   const open = Boolean(anchorEl);
 
-  const date = event.start.toDateString();
-  const startTime = event.start.toLocaleTimeString([], { timeStyle: "short" });
-  const endTime = event.end.toLocaleTimeString([], { timeStyle: "short" });
-  const location = event.extendedProps.location;
+  const date = event?.start?.toDateString();
+  const startTime = event?.start?.toLocaleTimeString([], {
+    timeStyle: "short",
+  });
+  const endTime = event?.end?.toLocaleTimeString([], { timeStyle: "short" });
+  const location = event?.extendedProps?.location;
 
   return (
     <Popover
@@ -23,8 +26,8 @@ function EventDetails({ anchorEl, handleClose, event }) {
         horizontal: "right",
       }}
     >
-      <Grid container direction="row" sx={{ padding: 2 }}>
-        <Grid item xs={10}>
+      <Grid container direction="row" columnSpacing={3} sx={{ padding: 2, pr: 1  }}>
+        <Grid item xs={8} sx={{mt: 0.5}}>
           <Stack direction="column" spacing={1}>
             <Typography variant="h4">{event.title}</Typography>
             <Typography>
@@ -41,7 +44,22 @@ function EventDetails({ anchorEl, handleClose, event }) {
             </Typography>
           </Stack>
         </Grid>
-        <Grid item xs={2}></Grid>
+        <Grid item xs={4}>
+          <Stack direction="row" justifyContent="flex-end">
+          <IconButton sx={{ fontSize: "20px"  }} onClick={handleClose}>
+              <CloseOutlined />
+            </IconButton>
+          <IconButton sx={{ fontSize: "20px"  }} onClick={handleClose}>
+              <CloseOutlined />
+            </IconButton>
+          <IconButton sx={{ fontSize: "20px"  }} onClick={handleClose}>
+              <CloseOutlined />
+            </IconButton>
+            <IconButton sx={{ fontSize: "20px"  }} onClick={handleClose}>
+              <CloseOutlined />
+            </IconButton>
+          </Stack>
+        </Grid>
       </Grid>
     </Popover>
   );
