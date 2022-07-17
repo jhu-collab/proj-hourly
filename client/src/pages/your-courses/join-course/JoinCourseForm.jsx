bc2364ff2df0d87e1713fe747b9c11ee83ab5fc4
Samuel Atefah <satefah1@jhu.edu>
2022-07-17 15:04:43 -0400

    Imports: changed mui imports to mnimize bundle size

--- client/src/pages/your-courses/join-course/JoinCourseForm.jsx
+++ client/src/pages/your-courses/join-course/JoinCourseForm.jsx
@@ -1,6 +1,6 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import Button from "@mui/material";
-import Stack from "@mui/material";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import Form from "../../../components/form-ui/Form";
