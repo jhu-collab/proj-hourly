a1e826348c46ea5567174a9caac22f9adc8b64f3
Samuel Atefah <satefah1@jhu.edu>
2022-07-22 18:36:40 -0400

    EventPopover: made icon buttons conditional for student/staff

--- client/src/pages/calendar/event-details/EventPopover.jsx
+++ client/src/pages/calendar/event-details/EventPopover.jsx
@@ -7,6 +7,8 @@ import Stack from "@mui/material/Stack";
 import DeleteAction from "./DeleteAction";
 import EventDetails from "./EventDetails";
 import EditAction from "./EditAction";
+import useStore from "../../../services/store";
+import { Button, Typography } from "@mui/material";
 
 /**
  * The popover the is rendered when a calendar event is clicked on
@@ -15,6 +17,8 @@ import EditAction from "./EditAction";
  * @returns a popover display event information.
  */
 function EventPopover({ anchorEl, handleClose }) {
+  const { courseType } = useStore();
+
   return (
     <Popover
       open={Boolean(anchorEl)}
@@ -40,17 +44,25 @@ function EventPopover({ anchorEl, handleClose }) {
         </Grid>
         <Grid item xs={4}>
           <Stack direction="row" justifyContent="flex-end">
-            <IconButton sx={{ fontSize: "20px" }}>
+            {courseType === "staff" && <IconButton sx={{ fontSize: "20px" }}>
               <InfoCircleOutlined />
-            </IconButton>
-            <EditAction handlePopoverClose={handleClose} />
-            <DeleteAction handlePopoverClose={handleClose} />
+            </IconButton>}
+            {courseType === "staff" && (
+              <EditAction handlePopoverClose={handleClose} />
+            )}
+            {courseType === "staff" && (
+              <DeleteAction handlePopoverClose={handleClose} />
+            )}
             <IconButton sx={{ fontSize: "20px" }} onClick={handleClose}>
               <CloseOutlined />
             </IconButton>
           </Stack>
         </Grid>
       </Grid>
+      <Stack alignItems="center" spacing={2}>
+        <Typography color="red" paddingX={2}>You are not registered for this session</Typography>
+        <Button variant="contained" fullWidth>Sign Up</Button>
+      </Stack>
     </Popover>
   );
 }
