050da033ebf0c7697f4610052dea1a2f2ae62d95
Samuel Atefah <satefah1@jhu.edu>
2022-07-29 13:12:04 -0400

    Code styling: ran prettier

--- client/src/services/store.js
+++ client/src/services/store.js
@@ -2,27 +2,33 @@ import create from "zustand";
 import { persist } from "zustand/middleware";
 
 // Manages states that involve layout control
-export const useLayoutStore = create(persist((set) => ({
-  openSidebar: false,
-  toggleOpenSidebar: (value) =>
-    set((state) => ({
-      openSidebar: value || !state.openSidebar,
-    })),
-  selectedSidebarItem: "your-courses",
-  selectSidebarItem: (value) =>
-    set(() => ({
-      selectedSidebarItem: value,
-    })),
-  courseType: "student",
-  toggleCourseType: (value) =>
-    set((state) => ({
-      courseType: value || (state.courseType === "staff" ? "student" : "staff"),
-    })),
-}),{
-  name: "layout",
-  getStorage: () => localStorage,
-  partialize: (state) => ({ courseType: state.courseType }),
-}));
+export const useLayoutStore = create(
+  persist(
+    (set) => ({
+      openSidebar: false,
+      toggleOpenSidebar: (value) =>
+        set((state) => ({
+          openSidebar: value || !state.openSidebar,
+        })),
+      selectedSidebarItem: "your-courses",
+      selectSidebarItem: (value) =>
+        set(() => ({
+          selectedSidebarItem: value,
+        })),
+      courseType: "student",
+      toggleCourseType: (value) =>
+        set((state) => ({
+          courseType:
+            value || (state.courseType === "staff" ? "student" : "staff"),
+        })),
+    }),
+    {
+      name: "layout",
+      getStorage: () => localStorage,
+      partialize: (state) => ({ courseType: state.courseType }),
+    }
+  )
+);
 
 // Manages states that involve theme control
 export const useThemeStore = create(
