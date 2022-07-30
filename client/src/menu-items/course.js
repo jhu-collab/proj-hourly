4ab0c2f2abe243b0eabaf5a97509db9500d30585
Tarık Can Yıldırım <tarky@tarks-air.acasmart.jh.edu>
2022-07-29 21:30:08 -0400

    editing TODO comments

--- client/src/menu-items/course.js
+++ client/src/menu-items/course.js
@@ -25,8 +25,8 @@ export const course = (currentCourse) => {
         id: "roster",
         title: "Roster",
         type: "item",
+        //TODO change it to roster once backend is done
         url: "/roster-test",
-        //TODO
         icon: icons.CalendarOutlined,
         breadcrumbs: false,
       },
