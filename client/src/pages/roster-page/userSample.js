a3b96c5af642a8ad094d23c39e1e8149c2556e79
Tarık Can Yıldırım <tarky@tarks-air.lan>
2022-08-04 20:07:37 -0400

    updated the fake data

--- client/src/pages/roster-page/userSample.js
+++ client/src/pages/roster-page/userSample.js
@@ -1,27 +1,22 @@
 export const userSample = {
   students: [
     {
-      username: "hi",
-      email: "hi@gmail.com",
+      username: "tarik",
+      email: "tarik@gmail.com",
       accountid: 1,
     },
   ],
   staff: [
     {
-      username: "bye",
-      email: "bye@gmail.com",
+      username: "sam",
+      email: "sam@gmail.com",
       accountid: 0,
     },
   ],
   instructors: [
     {
-      username: "no",
-      email: "no@gmail.com",
-      accountid: 2,
-    },
-    {
-      username: "no",
-      email: "no@gmail.com",
+      username: "ali",
+      email: "ali@gmail.com",
       accountid: 3,
     },
   ],
