81b75c05b20a7c12057ceb7945c003f7f6522bd5
Samuel Atefah <satefah1@jhu.edu>
2022-08-13 19:47:45 -0400

    Registrations Bar: filter icon changes when clicked

--- client/src/pages/registrations/Registration.jsx
+++ client/src/pages/registrations/Registration.jsx
@@ -1,12 +1,13 @@
-import { Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import React from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import DownOutlined from "@ant-design/icons/DownOutlined";
 import moment from "moment";
+import ConfirmPopup, { confirmDialog } from "../../components/ConfirmPopup";
 
-function Registration({ registration }) {
+function Registration({ registration, type }) {
   return (
     <Accordion sx={{ paddingX: 2, paddingY: 1 }}>
       <AccordionSummary expandIcon={<DownOutlined />}>
@@ -32,6 +33,22 @@ function Registration({ registration }) {
       <AccordionDetails>
         {/* TODO: Depending on what type of booking this event has been made for,
          details about the event will be provided here */}
+        {type === 0 && (
+          <>
+            <Button
+              variant="contained"
+              onClick={() => {
+                confirmDialog(
+                  "Do you want to cancel this registration?",
+                  () => console.log("Registration canceled.") // TODO: Need route to cancel event
+                );
+              }}
+            >
+              Cancel
+            </Button>
+            <ConfirmPopup />
+          </>
+        )}
       </AccordionDetails>
     </Accordion>
   );
--- client/src/pages/registrations/RegistrationsBar.jsx
+++ client/src/pages/registrations/RegistrationsBar.jsx
@@ -4,10 +4,17 @@ import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import TimeTabs from "./TimeTabs";
 import FilterOutlined from "@ant-design/icons/FilterOutlined";
+import FilterFilled from "@ant-design/icons/FilterFilled";
 import useTheme from "@mui/material/styles/useTheme";
+import { useState } from "react";
 
 function RegistrationsBar() {
   const theme = useTheme();
+  const [filter, setFilter] = useState(false);
+
+  const onClick = () => {
+    setFilter(!filter);
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -19,8 +26,13 @@ function RegistrationsBar() {
         <Toolbar>
           <TimeTabs />
           <Box sx={{ flexGrow: 1 }} />
-          <IconButton>
-            <FilterOutlined style={{ color: theme.palette.primary.main }} />
+          {/* TODO: Need to create a filter panel for registrations */}
+          <IconButton onClick={onClick}>
+            {filter ? (
+              <FilterFilled style={{ color: theme.palette.primary.main }} />
+            ) : (
+              <FilterOutlined style={{ color: theme.palette.primary.main }} />
+            )}
           </IconButton>
         </Toolbar>
       </AppBar>
