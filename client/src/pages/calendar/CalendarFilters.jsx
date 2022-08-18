c9934a23ffb87c6de4b9a369e01d0f7726226d94
Samuel Atefah <satefah1@jhu.edu>
2022-08-18 06:12:43 -0400

    Calendar: utilized Accordion and RadioGroup componetns to create filter

--- client/src/pages/calendar/CalendarFilters.jsx
+++ client/src/pages/calendar/CalendarFilters.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import DownOutlined from "@ant-design/icons/DownOutlined";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
 function CalendarFilters() {
+  const [filter, setFilter] = useState("myEvents");
+  const [expanded, setExpanded] = useState(true);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const handleExpandedChange = (event, isExpanded) => {
+    setExpanded(isExpanded);
+  };
+
   return (
-    <div>CalendarFilters</div>
-  )
+    <Accordion elevation={0} expanded={expanded} onChange={handleExpandedChange}>
+      <AccordionSummary expandIcon={<DownOutlined />}>
+        <Typography variant="h5" fontWeight={600}>
+          Filters
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <RadioGroup
+          value={filter}
+          onChange={handleFilterChange}
+        >
+          <FormControlLabel value="myEvents" control={<Radio />} label="My Events" />
+          <FormControlLabel value="allEvents" control={<Radio />} label="All Events" />
+          <FormControlLabel value="registrations" control={<Radio />} label="Registrations" />
+        </RadioGroup>
+      </AccordionDetails>
+    </Accordion>
+  );
 }
 
-export default CalendarFilters
+export default CalendarFilters;
