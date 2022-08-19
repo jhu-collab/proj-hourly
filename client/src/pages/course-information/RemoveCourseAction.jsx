8c3c1ab28fd640edd1412526925717903a0622fd
varun-harish <82960805+varun-harish@users.noreply.github.com>
2022-08-19 17:19:57 -0400

    useQuery

--- client/src/pages/course-information/RemoveCourseAction.jsx
+++ client/src/pages/course-information/RemoveCourseAction.jsx
@@ -7,6 +7,8 @@ import { toast } from "react-toastify";
 import ConfirmPopup, { confirmDialog } from "../../components/ConfirmPopup";
 import Loader from "../../components/Loader";
 import { useEventStore } from "../../services/store";
+import { useNavigate } from "react-router-dom";
+import { errorToast } from "../../utils/toasts";
 import { leaveCourse } from "../../utils/requests";
 
 /**
@@ -16,16 +18,26 @@ import { leaveCourse } from "../../utils/requests";
  * @returns Delete action button and confirmation popup.
  */
 function RemoveCourseAction({ courseid }) {
+  const queryClient = useQueryClient();
 
+  const { mutate } = useMutation(leaveCourse(courseid), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["courses"]);
+      toast.success(`Successfully removed course!`);
+    },
+    onError: (error) => {
+      errorToast(error);
+    },
+  });
   return (
     <>
     <IconButton
         sx={{ fontSize: "50px", bottom: 60, left: '93%', }}
         onClick={() => {
           confirmDialog("Do you really want to remove this course?", () => {
-              leaveCourse(courseid);
-              toast.success(`Successfully removed course!`);
+            mutate(courseid);
           });
+         
         }}
       >
         <DeleteOutlined />
