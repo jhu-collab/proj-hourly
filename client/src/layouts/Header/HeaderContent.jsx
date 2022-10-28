eeca5e81b1b1a4179fb432f505baa27bff97d2a8
Samuel Atefah <satefah1@jhu.edu>
2022-10-28 07:53:18 -0400

    AppBar: added Settings Icon

--- client/src/layouts/Header/HeaderContent.jsx
+++ client/src/layouts/Header/HeaderContent.jsx
@@ -1,13 +1,14 @@
-import Box from "@mui/material/Box";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Profile from "./Profile";
 import Notification from "./Notification";
 import MobileSection from "./MobileSection";
+import Settings from "./Settings";
 
 function HeaderContent() {
   const matchesXs = useMediaQuery((theme) => theme.breakpoints.down("md"));
   return (
     <>
+      <Settings />
       <Notification />
       {!matchesXs && <Profile />}
       {matchesXs && <MobileSection />}
