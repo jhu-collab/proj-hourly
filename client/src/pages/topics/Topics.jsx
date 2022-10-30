a04ef4cd62a4952c3c263853bdba7e1dbe0908b0
Samuel Atefah <satefah1@jhu.edu>
2022-10-30 11:17:54 -0400

    Topics: added create button

--- client/src/pages/topics/Topics.jsx
+++ client/src/pages/topics/Topics.jsx
@@ -1,7 +1,23 @@
+import Fab from "@mui/material/Fab";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 import React from "react";
 
 function Topics() {
-  return <div>Topics</div>;
+  return (
+    <>
+      <Fab
+        color="primary"
+        onClick={() => NiceModal.show("create-registration-type")}
+        sx={{
+          position: "fixed",
+          bottom: (theme) => theme.spacing(3),
+          right: (theme) => theme.spacing(3),
+        }}
+      >
+        <SpeedDialIcon />
+      </Fab>
+    </>
+  );
 }
 
 export default Topics;
