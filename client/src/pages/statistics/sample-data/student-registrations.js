f063bed5f516464451f900193d9dc91b5babfff8
Samuel Atefah <satefah1@jhu.edu>
2022-11-12 22:22:59 -0500

    Statistics: created sample data for student-registrations

---
+++ client/src/pages/statistics/sample-data/student-registrations.js
@@ -0,0 +1,401 @@
+export const sampleStudentRegistration = [{
+    "id": 1,
+    "name": "Ive Toyne",
+    "registrations": 15
+  }, {
+    "id": 2,
+    "name": "Mendel McGowan",
+    "registrations": 59
+  }, {
+    "id": 3,
+    "name": "Brett Sandlin",
+    "registrations": 36
+  }, {
+    "id": 4,
+    "name": "Tracie Sennett",
+    "registrations": 54
+  }, {
+    "id": 5,
+    "name": "Loralyn Fishley",
+    "registrations": 62
+  }, {
+    "id": 6,
+    "name": "Katee Studdard",
+    "registrations": 45
+  }, {
+    "id": 7,
+    "name": "Ilka Mebs",
+    "registrations": 55
+  }, {
+    "id": 8,
+    "name": "Beverley Pryer",
+    "registrations": 74
+  }, {
+    "id": 9,
+    "name": "Bernice Leveritt",
+    "registrations": 15
+  }, {
+    "id": 10,
+    "name": "Vince Dungate",
+    "registrations": 31
+  }, {
+    "id": 11,
+    "name": "Huntley Osbidston",
+    "registrations": 22
+  }, {
+    "id": 12,
+    "name": "Astrid McVaugh",
+    "registrations": 60
+  }, {
+    "id": 13,
+    "name": "Horace But",
+    "registrations": 3
+  }, {
+    "id": 14,
+    "name": "Janetta Klugel",
+    "registrations": 69
+  }, {
+    "id": 15,
+    "name": "Jake Venart",
+    "registrations": 92
+  }, {
+    "id": 16,
+    "name": "Ingaborg Marvel",
+    "registrations": 73
+  }, {
+    "id": 17,
+    "name": "Milzie Chat",
+    "registrations": 29
+  }, {
+    "id": 18,
+    "name": "Peggie MacTrustey",
+    "registrations": 95
+  }, {
+    "id": 19,
+    "name": "Dennie Hosburn",
+    "registrations": 5
+  }, {
+    "id": 20,
+    "name": "Colman Cleminshaw",
+    "registrations": 43
+  }, {
+    "id": 21,
+    "name": "Sadie Moar",
+    "registrations": 68
+  }, {
+    "id": 22,
+    "name": "Gradeigh Ladson",
+    "registrations": 43
+  }, {
+    "id": 23,
+    "name": "Antone Fall",
+    "registrations": 82
+  }, {
+    "id": 24,
+    "name": "Wiatt Matczak",
+    "registrations": 49
+  }, {
+    "id": 25,
+    "name": "Ellery Baglan",
+    "registrations": 62
+  }, {
+    "id": 26,
+    "name": "Olvan Karet",
+    "registrations": 29
+  }, {
+    "id": 27,
+    "name": "Lorna Burgot",
+    "registrations": 81
+  }, {
+    "id": 28,
+    "name": "Pamelina Smelley",
+    "registrations": 22
+  }, {
+    "id": 29,
+    "name": "Ferne Betje",
+    "registrations": 10
+  }, {
+    "id": 30,
+    "name": "Klarrisa Hinckes",
+    "registrations": 98
+  }, {
+    "id": 31,
+    "name": "Tildy Matessian",
+    "registrations": 10
+  }, {
+    "id": 32,
+    "name": "Oliviero De Pero",
+    "registrations": 3
+  }, {
+    "id": 33,
+    "name": "Sayres Ainscough",
+    "registrations": 73
+  }, {
+    "id": 34,
+    "name": "Gisella Muxworthy",
+    "registrations": 57
+  }, {
+    "id": 35,
+    "name": "Hammad Celes",
+    "registrations": 40
+  }, {
+    "id": 36,
+    "name": "Ike Kilby",
+    "registrations": 90
+  }, {
+    "id": 37,
+    "name": "Maury Grzegorczyk",
+    "registrations": 22
+  }, {
+    "id": 38,
+    "name": "Byran Resun",
+    "registrations": 66
+  }, {
+    "id": 39,
+    "name": "Vilhelmina Sully",
+    "registrations": 19
+  }, {
+    "id": 40,
+    "name": "Madel Beardsdale",
+    "registrations": 83
+  }, {
+    "id": 41,
+    "name": "Killian Picot",
+    "registrations": 91
+  }, {
+    "id": 42,
+    "name": "Brnaba Lympenie",
+    "registrations": 2
+  }, {
+    "id": 43,
+    "name": "Lea Fice",
+    "registrations": 98
+  }, {
+    "id": 44,
+    "name": "Wren Deam",
+    "registrations": 65
+  }, {
+    "id": 45,
+    "name": "Laurene Lux",
+    "registrations": 81
+  }, {
+    "id": 46,
+    "name": "Garrek Rutter",
+    "registrations": 77
+  }, {
+    "id": 47,
+    "name": "Lisa Launchbury",
+    "registrations": 37
+  }, {
+    "id": 48,
+    "name": "Van Stoneman",
+    "registrations": 16
+  }, {
+    "id": 49,
+    "name": "Rossy Ivison",
+    "registrations": 74
+  }, {
+    "id": 50,
+    "name": "Korie Dawby",
+    "registrations": 26
+  }, {
+    "id": 51,
+    "name": "Mureil Berrygun",
+    "registrations": 13
+  }, {
+    "id": 52,
+    "name": "Dyann Hulls",
+    "registrations": 99
+  }, {
+    "id": 53,
+    "name": "Thor Scotter",
+    "registrations": 9
+  }, {
+    "id": 54,
+    "name": "Jonah Shackleton",
+    "registrations": 29
+  }, {
+    "id": 55,
+    "name": "Laverne McCloch",
+    "registrations": 70
+  }, {
+    "id": 56,
+    "name": "Leif Brookbank",
+    "registrations": 85
+  }, {
+    "id": 57,
+    "name": "Cal Brittle",
+    "registrations": 77
+  }, {
+    "id": 58,
+    "name": "Flore Massimo",
+    "registrations": 74
+  }, {
+    "id": 59,
+    "name": "Norbert Gaskill",
+    "registrations": 87
+  }, {
+    "id": 60,
+    "name": "Allister Harkin",
+    "registrations": 56
+  }, {
+    "id": 61,
+    "name": "Lois Alred",
+    "registrations": 42
+  }, {
+    "id": 62,
+    "name": "Lucia Spadeck",
+    "registrations": 97
+  }, {
+    "id": 63,
+    "name": "Megen Waleworke",
+    "registrations": 29
+  }, {
+    "id": 64,
+    "name": "Jay Hitschke",
+    "registrations": 56
+  }, {
+    "id": 65,
+    "name": "Gregorio Hance",
+    "registrations": 77
+  }, {
+    "id": 66,
+    "name": "Caren Beaten",
+    "registrations": 27
+  }, {
+    "id": 67,
+    "name": "Bel Birwhistle",
+    "registrations": 46
+  }, {
+    "id": 68,
+    "name": "Nicolas Elfe",
+    "registrations": 78
+  }, {
+    "id": 69,
+    "name": "Roseann Gatlin",
+    "registrations": 87
+  }, {
+    "id": 70,
+    "name": "Paco Brinson",
+    "registrations": 31
+  }, {
+    "id": 71,
+    "name": "Emmey Aveling",
+    "registrations": 1
+  }, {
+    "id": 72,
+    "name": "Jo Brockherst",
+    "registrations": 84
+  }, {
+    "id": 73,
+    "name": "Joleen Janks",
+    "registrations": 40
+  }, {
+    "id": 74,
+    "name": "Alica Revelle",
+    "registrations": 83
+  }, {
+    "id": 75,
+    "name": "Shea Fitzroy",
+    "registrations": 57
+  }, {
+    "id": 76,
+    "name": "Sibel Wethered",
+    "registrations": 21
+  }, {
+    "id": 77,
+    "name": "Mallory Schober",
+    "registrations": 37
+  }, {
+    "id": 78,
+    "name": "Quillan De Luna",
+    "registrations": 97
+  }, {
+    "id": 79,
+    "name": "Kermie Punton",
+    "registrations": 29
+  }, {
+    "id": 80,
+    "name": "Nahum Shelly",
+    "registrations": 14
+  }, {
+    "id": 81,
+    "name": "Rourke Muffin",
+    "registrations": 62
+  }, {
+    "id": 82,
+    "name": "Ted Potte",
+    "registrations": 61
+  }, {
+    "id": 83,
+    "name": "Kingsly Enrietto",
+    "registrations": 75
+  }, {
+    "id": 84,
+    "name": "Svend Digg",
+    "registrations": 63
+  }, {
+    "id": 85,
+    "name": "Osborn Potticary",
+    "registrations": 20
+  }, {
+    "id": 86,
+    "name": "Tyson Malby",
+    "registrations": 77
+  }, {
+    "id": 87,
+    "name": "Tamiko Franchioni",
+    "registrations": 45
+  }, {
+    "id": 88,
+    "name": "Arlyne Neaverson",
+    "registrations": 5
+  }, {
+    "id": 89,
+    "name": "Bambi Mawford",
+    "registrations": 39
+  }, {
+    "id": 90,
+    "name": "Glori Southerns",
+    "registrations": 86
+  }, {
+    "id": 91,
+    "name": "Kaia Stovold",
+    "registrations": 25
+  }, {
+    "id": 92,
+    "name": "Ted Oglesbee",
+    "registrations": 60
+  }, {
+    "id": 93,
+    "name": "Tonie Semarke",
+    "registrations": 5
+  }, {
+    "id": 94,
+    "name": "Salomone Crennan",
+    "registrations": 76
+  }, {
+    "id": 95,
+    "name": "Erinna Brisset",
+    "registrations": 20
+  }, {
+    "id": 96,
+    "name": "Kip Steinham",
+    "registrations": 39
+  }, {
+    "id": 97,
+    "name": "Galven Lobell",
+    "registrations": 9
+  }, {
+    "id": 98,
+    "name": "Gipsy Smithies",
+    "registrations": 14
+  }, {
+    "id": 99,
+    "name": "Ches Vasyutin",
+    "registrations": 67
+  }, {
+    "id": 100,
+    "name": "Ricky Clewlowe",
+    "registrations": 70
+  }];
