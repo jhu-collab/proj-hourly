cdb7774fe51f68ab9fcc3da43e19e7580398d259
Samuel Atefah <satefah1@jhu.edu>
2022-12-07 05:58:54 -0500

    UI: small fix to button

--- client/src/pages/calendar/calendar-menu/CalendarViews.jsx
+++ client/src/pages/calendar/calendar-menu/CalendarViews.jsx
@@ -51,7 +51,11 @@ function CalendarViews({ calendarRef }) {
         view by
       </Typography>
       <ToggleButtonGroup
-        sx={{ bgcolor: "tertiary.main" }}
+        sx={{
+          bgcolor: "tertiary.main",
+          boxShadow:
+            "0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)",
+        }}
         value={alignment}
         exclusive
         fullWidth
