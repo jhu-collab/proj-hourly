12ef0ef337f8a04418bdd8e8fc8f7a06b59df31d
varun-harish <82960805+varun-harish@users.noreply.github.com>
2022-12-30 12:26:31 -0500

    disabled button for staff

--- client/src/pages/roster/ChangeRoleIcon.jsx
+++ client/src/pages/roster/ChangeRoleIcon.jsx
@@ -1,8 +1,11 @@
 import NiceModal from "@ebay/nice-modal-react";
 import { GridActionsCellItem } from "@mui/x-data-grid";
 import UserSwitchOutlined from "@ant-design/icons/UserSwitchOutlined";
+import useQueryMyRole from "../../hooks/useQueryMyRole";
+
 function ChangeRoleIcon(props) {
   const { params, isStaff } = props;
+  const { data } = useQueryMyRole();
   return (
     <>
       <GridActionsCellItem
@@ -10,6 +13,7 @@ function ChangeRoleIcon(props) {
         onClick={() => {
           NiceModal.show("change-user-role", { params: params, isStaff: isStaff });
         }}
+        disabled={data?.role !== "Instructor"}
         label="Change Role"
         size="large"
       />
