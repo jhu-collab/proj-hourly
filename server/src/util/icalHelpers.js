9b0fb2fa040499413d65151a353d52993b66ac88
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-01-28 19:31:55 -0500

    Adding logs

--- server/src/util/icalHelpers.js
+++ server/src/util/icalHelpers.js
@@ -123,6 +123,8 @@ export const generateRecurringEventJson = (officeHour) => {
       timezone: "America/New_York",
     })
   );
+  console.log(start);
+  console.log(end);
   while (start < end) {
     let notCancelled = true;
     for (const date of officeHour.isCancelledOn) {
@@ -136,6 +138,9 @@ export const generateRecurringEventJson = (officeHour) => {
       currEnd.setUTCHours(end.getUTCHours());
       currEnd.setUTCMinutes(end.getUTCMinutes());
       currEnd.setUTCSeconds(end.getUTCSeconds());
+      console.log("offset");
+      console.log(end.getTimezoneOffset());
+      console.log(start.getTimezoneOffset());
       if (end.getTimezoneOffset() !== start.getTimezoneOffset()) {
         currEnd.setUTCHours(
           currEnd.getUTCHours() +
