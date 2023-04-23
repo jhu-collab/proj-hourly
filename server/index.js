fb4075e1abf8dad0a60e26cac5a88280142c7ae8
will tong <46694175+willtong77@users.noreply.github.com>
2023-04-22 20:51:49 -0400

    undo port change

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import app from "./src/index.js";
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Express app listening at port: http://localhost:${port}/`);
