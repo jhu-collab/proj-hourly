3fe8fd3cd7a95eb787fc8f2d4ffec92a66586c9f
Mark Tiavises <mtiavis1@jhu.edu>
2023-04-23 20:05:47 -0400

    Renaming file and attempting to change date of office hours

--- client/cypress/e2e/calendar-page/calendarStaff.cy.js
+++ client/cypress/e2e/calendar-page/calendarStaff.cy.js
@@ -1,5 +1,15 @@
-describe("Calendar Page: Create Office Hours", () => {
+const formatCypressDate = (date) => {
+  const year = date.toLocaleString("default", { year: "numeric" });
+  const month = date.toLocaleString("default", { month: "2-digit" });
+  const day = date.toLocaleString("default", { day: "2-digit" });
+
+  // Generate yyyy-mm-dd date string
+  return year + "-" + month + "-" + day;
+};
+describe("Calendar Page: Staff Office Hours", () => {
   const BASE_URL = "http://localhost:3000/";
+  //Set the date used for the tests
+  let now = new Date();
 
   before(() => {
     cy.task("removeOH", "AVENGE");
@@ -9,7 +19,7 @@ describe("Calendar Page: Create Office Hours", () => {
     cy.task("removeOH", "AVENGE");
   });
 
-  describe.skip("creating office hours", () => {
+  describe("creating office hours", () => {
     beforeEach(() => {
       cy.visit(BASE_URL);
       cy.get('input[id=":r1:"]').type("thor");
@@ -23,7 +33,7 @@ describe("Calendar Page: Create Office Hours", () => {
       //remove any office hours that were created in the making of the tests
       cy.task("removeOH", "AVENGE");
     });
-    it("staff can create office hours", () => {
+    it.skip("staff can create office hours", () => {
       //Look one week ahead because want to be able to run these tests anytime
       //since cannot make office hours in the past
       cy.get('button[title="Next week"]').should("be.visible").click();
@@ -67,6 +77,35 @@ describe("Calendar Page: Create Office Hours", () => {
       cy.get('[data-cy="location-text"]')
         .should("be.visible")
         .should("have.text", "Location: " + locationName);
+    });
+
+    it.skip("staff can create recurring office hours", () => {
+      //Look one week ahead because want to be able to run these tests anytime
+      //since cannot make office hours in the past
+      cy.get('button[title="Next week"]').should("be.visible").click();
+
+      //This is extremely hard coded, trying to click a time on tuesday
+      //It seems difficult to put a data-cy tag on a element because it's
+      //a premade calendar
+      cy.xpath(
+        "/html/body/div[1]/div[2]/main/div[2]/div/div/div/div[2]/div/table/tbody/tr/td/div/div/div/div[2]/table/tbody/tr/td[4]/div"
+      ).click({ force: true });
+
+      cy.get('input[id=":r7:"]').should("have.value", "11:30");
+      cy.get('input[id=":r9:"]').should("have.value", "12:00");
+
+      cy.get('input[id=":rb:"]').type("2023-04-25");
+
+      const locationName = "Mark's Location";
+
+      cy.get('[data-cy="create-location-input"]')
+        .should("be.visible")
+        .type(locationName);
+      cy.get('[data-cy="create-event-submit"]').should("be.visible").click();
+
+      cy.reload();
+      cy.get('button[title="Next week"]').should("be.visible").click();
+      cy.get("[data-cy^=event-]").should("have.length", 1).click();
     });
   });
 
@@ -118,8 +157,9 @@ describe("Calendar Page: Create Office Hours", () => {
     });
   });
 
-  describe("Staff can edit office hours", () => {
+  describe("editing office hours", () => {
     beforeEach(() => {
+      now = new Date();
       cy.task("removeOH", "AVENGE");
 
       cy.visit(BASE_URL);
@@ -154,7 +194,7 @@ describe("Calendar Page: Create Office Hours", () => {
       cy.get('button[title="Next week"]').should("be.visible").click();
     });
 
-    it("edit location", () => {
+    it.skip("edit location", () => {
       cy.get("[data-cy^=event-]").should("have.length", 1).click();
       cy.get("[data-cy=edit-action-icon]").click();
 
@@ -186,6 +226,64 @@ describe("Calendar Page: Create Office Hours", () => {
       cy.get('[data-cy="location-text"]')
         .should("be.visible")
         .should("have.text", "Location: " + newLocationName);
+    });
+
+    it.skip("edit time", () => {
+      cy.get("[data-cy^=event-]").should("have.length", 1).click();
+      cy.get("[data-cy=edit-action-icon]").click();
+
+      //The actual edit form should exist
+      cy.get('[data-cy="edit-event-form"]').should("be.visible");
+
+      //Edit to start at 9:30 am
+      cy.get('[data-cy="edit-start-time-text"]')
+        .should("be.visible")
+        .type("21:30");
+      //Edit to end at 11:30 am
+      cy.get('[data-cy="edit-end-time-text"]')
+        .should("be.visible")
+        .type("23:30");
+
+      cy.get('[data-cy="edit-event-submit"').should("be.visible").click();
+
+      cy.get("[data-cy^=event-]").should("have.length", 1);
+      cy.get("[data-cy^=event-]").click();
+
+      //Test that the Date is mostly correct on Tuesday
+      cy.get('[data-cy="date-text"]')
+        .should("be.visible")
+        .contains("Date: Tue");
+
+      //Test that the time did change
+      cy.get('[data-cy="time-text"]')
+        .should("be.visible")
+        .should("have.text", "Time: 9:30 PM - 11:30 PM");
+
+      const locationName = "Mark's Location";
+
+      //Test that the location did not change
+      cy.get('[data-cy="location-text"]')
+        .should("be.visible")
+        .should("have.text", "Location: " + locationName);
+    });
+
+    //TODO: Broken
+    it("edit day", () => {
+      cy.get("[data-cy^=event-]").should("have.length", 1).click();
+      cy.get("[data-cy=edit-action-icon]").click();
+
+      //The actual edit form should exist
+      cy.get('[data-cy="edit-event-form"]').should("be.visible");
+
+      cy.log(formatCypressDate(now));
+      //Edit to start at 9  :30 am
+      now.setDate(now.getDate() + 8);
+      const nowStr = formatCypressDate(now);
+
+      //TODO: This is broken.
+      //It is very hard to change the date when creating/editing an event
+      cy.log(nowStr);
+      cy.get('input[id=":r9:"]').clear().type(`${nowStr}`);
     });
   });
 });
