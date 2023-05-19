aca9904ae1beb08bc785bad689f54833a5c1d789
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-05-19 18:04:32 -0400

    fixing tests for invalid DOW and removing phone number tests

--- server/tests/routes/accounts.test.js
+++ server/tests/routes/accounts.test.js
@@ -9,7 +9,10 @@ const request = supertest(app);
 const endpoint = "/api/account";
 
 describe(`Test endpoint ${endpoint}`, () => {
-  let user, deleteUser, admin, deleteAdmin = {};
+  let user,
+    deleteUser,
+    admin,
+    deleteAdmin = {};
   let accountIds = [];
 
   beforeAll(async () => {
@@ -17,37 +20,37 @@ describe(`Test endpoint ${endpoint}`, () => {
       data: {
         userName: "Test User",
         email: "user@test.io",
-        role: Role.User
-      }
+        role: Role.User,
+      },
     });
-    user = {...user, token: createToken({ user: user })};
+    user = { ...user, token: createToken({ user: user }) };
 
     deleteUser = await prisma.account.create({
       data: {
         userName: "Test Delete User",
         email: "deletemeuser@test.io",
-        role: Role.User
-      }
+        role: Role.User,
+      },
     });
-    deleteUser = {...deleteUser, token: createToken({ user: deleteUser })};
+    deleteUser = { ...deleteUser, token: createToken({ user: deleteUser }) };
 
     admin = await prisma.account.create({
       data: {
         userName: "Test Admin",
         email: "admin@test.io",
-        role: Role.Admin
-      }
+        role: Role.Admin,
+      },
     });
-    admin = {...admin, token: createToken({ user: admin })};
+    admin = { ...admin, token: createToken({ user: admin }) };
 
     deleteAdmin = await prisma.account.create({
       data: {
         userName: "Test Delete Admin",
         email: "deletemeadmin@test.io",
-        role: Role.Admin
-      }
+        role: Role.Admin,
+      },
     });
-    deleteAdmin = {...deleteAdmin, token: createToken({ user: deleteAdmin })};
+    deleteAdmin = { ...deleteAdmin, token: createToken({ user: deleteAdmin }) };
 
     accountIds = [user.id, deleteUser.id, admin.id, deleteAdmin.id];
   });
@@ -56,10 +59,10 @@ describe(`Test endpoint ${endpoint}`, () => {
     await prisma.account.deleteMany({
       where: {
         id: {
-          in: accountIds
-        }
-      }
-    })
+          in: accountIds,
+        },
+      },
+    });
   });
 
   describe(`Test POST: ${endpoint}/signup`, async () => {
@@ -67,95 +70,87 @@ describe(`Test endpoint ${endpoint}`, () => {
       const body = {
         email: "email1@test.io",
         name: "test name1",
-      }
-      const response = await request
-        .post(`${endpoint}/signup`)
-        .send(body);
+      };
+      const response = await request.post(`${endpoint}/signup`).send(body);
       expect(response.statusCode).toBe(201);
       const user = await prisma.account.findFirst({
         where: {
-          email: "email1@test.io"
-        }
+          email: "email1@test.io",
+        },
       });
       expect(user).toBeDefined();
       expect(user.userName).toEqual("test name1");
       accountIds.push(user.id);
     });
 
-    it("Return 201 with phone number", async () => {
-      const body = {
-        email: "email2@test.io",
-        phoneNumber: "11111111111",
-        name: "test name2",
-      }
-      const response = await request
-        .post(`${endpoint}/signup`)
-        .send(body);
-      expect(response.statusCode).toBe(201);
-      const user = await prisma.account.findFirst({
-        where: {
-          email: "email2@test.io"
-        }
-      });
-      expect(user).toBeDefined();
-      expect(user.userName).toEqual("test name2");
-      expect(user.phoneNumber).toEqual("11111111111");
-      accountIds.push(user.id);
-    });
+    // it("Return 201 with phone number", async () => {
+    //   const body = {
+    //     email: "email2@test.io",
+    //     phoneNumber: "11111111111",
+    //     name: "test name2",
+    //   }
+    //   const response = await request
+    //     .post(`${endpoint}/signup`)
+    //     .send(body);
+    //   expect(response.statusCode).toBe(201);
+    //   const user = await prisma.account.findFirst({
+    //     where: {
+    //       email: "email2@test.io"
+    //     }
+    //   });
+    //   expect(user).toBeDefined();
+    //   expect(user.userName).toEqual("test name2");
+    //   expect(user.phoneNumber).toEqual("11111111111");
+    //   accountIds.push(user.id);
+    // });
 
     it("Return 409 when email is not unique", async () => {
       const body = {
         email: "user@test.io",
         name: "test name3",
-      }
-      const response = await request
-        .post(`${endpoint}/signup`)
-        .send(body);
+      };
+      const response = await request.post(`${endpoint}/signup`).send(body);
       expect(response.statusCode).toBe(409);
     });
 
-    it("Return 409 when phone number is not unique", async () => {
-      const body = {
-        email: "email3@test.io",
-        phoneNumber: "11111111111",
-        name: "test name3",
-      }
-      const response = await request
-        .post(`${endpoint}/signup`)
-        .send(body);
-      expect(response.statusCode).toBe(409);
-    });
+    // it("Return 409 when phone number is not unique", async () => {
+    //   const body = {
+    //     email: "email3@test.io",
+    //     phoneNumber: "11111111111",
+    //     name: "test name3",
+    //   }
+    //   const response = await request
+    //     .post(`${endpoint}/signup`)
+    //     .send(body);
+    //   expect(response.statusCode).toBe(409);
+    // });
 
-    it("Return 400 when phone number is invalid", async () => {
-      const body = {
-        email: "email3@test.io",
-        phoneNumber: "11111",
-        name: "test name3",
-      }
-      const response = await request
-        .post(`${endpoint}/signup`)
-        .send(body);
-      expect(response.statusCode).toBe(400);
-    });
+    // it("Return 400 when phone number is invalid", async () => {
+    //   const body = {
+    //     email: "email3@test.io",
+    //     phoneNumber: "11111",
+    //     name: "test name3",
+    //   }
+    //   const response = await request
+    //     .post(`${endpoint}/signup`)
+    //     .send(body);
+    //   expect(response.statusCode).toBe(400);
+    // });
 
     it("Return 400 when email is invalid", async () => {
       const body = {
         email: "email3",
         name: "test name3",
-      }
-      const response = await request
-        .post(`${endpoint}/signup`)
-        .send(body);
+      };
+      const response = await request.post(`${endpoint}/signup`).send(body);
       expect(response.statusCode).toBe(400);
     });
 
     it("Return 400 when name is empty", async () => {
       const body = {
         email: "email3@test.io",
-      }
-      const response = await request
-        .post(`${endpoint}/signup`)
-        .send(body);
+      };
+      const response = await request.post(`${endpoint}/signup`).send(body);
       expect(response.statusCode).toBe(400);
     });
   });
@@ -164,9 +159,7 @@ describe(`Test endpoint ${endpoint}`, () => {
       const body = {
         email: "user@test.io",
       };
-      const response = await request
-        .post(`${endpoint}/login`)
-        .send(body);
+      const response = await request.post(`${endpoint}/login`).send(body);
       expect(response.statusCode).toBe(202);
     });
 
@@ -174,9 +167,7 @@ describe(`Test endpoint ${endpoint}`, () => {
       const body = {
         email: "admin@test.io",
       };
-      const response = await request
-        .post(`${endpoint}/login`)
-        .send(body);
+      const response = await request.post(`${endpoint}/login`).send(body);
       expect(response.statusCode).toBe(202);
     });
 
@@ -184,9 +175,7 @@ describe(`Test endpoint ${endpoint}`, () => {
       const body = {
         email: "blah@test.io",
       };
-      const response = await request
-        .post(`${endpoint}/login`)
-        .send(body);
+      const response = await request.post(`${endpoint}/login`).send(body);
       expect(response.statusCode).toBe(403);
     });
   });
@@ -195,15 +184,12 @@ describe(`Test endpoint ${endpoint}`, () => {
     it("Return 202 when deleting a user account", async () => {
       const response = await request
         .delete(`${endpoint}/${deleteUser.id}`)
-        .set(
-          "Authorization",
-          "Bearer " + deleteUser.token
-        );
+        .set("Authorization", "Bearer " + deleteUser.token);
       expect(response.statusCode).toBe(202);
       const deleted = await prisma.account.findFirst({
         where: {
-          id: deleteUser.id
-        }
+          id: deleteUser.id,
+        },
       });
       expect(deleted).toBeNull();
     });
@@ -211,44 +197,34 @@ describe(`Test endpoint ${endpoint}`, () => {
     it("Return 202 when deleting an admin account", async () => {
       const response = await request
         .delete(`${endpoint}/${deleteAdmin.id}`)
-        .set(
-          "Authorization",
-          "Bearer " + deleteAdmin.token
-        );
+        .set("Authorization", "Bearer " + deleteAdmin.token);
       expect(response.statusCode).toBe(202);
       const deleted = await prisma.account.findFirst({
         where: {
-          id: deleteAdmin.id
-        }
+          id: deleteAdmin.id,
+        },
       });
       expect(deleted).toBeNull();
     });
 
     it("Return 401 when there's no token", async () => {
-      const response = await request
-        .delete(`${endpoint}/${deleteUser.id}`);
+      const response = await request.delete(`${endpoint}/${deleteUser.id}`);
       expect(response.statusCode).toBe(401);
     });
 
     it("Return 404 when id is invalid", async () => {
       const response = await request
         .delete(`${endpoint}/${deleteUser.id * 2}`)
-        .set(
-          "Authorization",
-          "Bearer " + deleteUser.token
-        );
+        .set("Authorization", "Bearer " + deleteUser.token);
       expect(response.statusCode).toBe(404);
-    })
+    });
   });
 
   describe(`Test GET: ${endpoint}/`, async () => {
     it("Return 202 when admin makes the request", async () => {
       const response = await request
         .get(`${endpoint}/`)
-        .set(
-          "Authorization",
-          "Bearer " + admin.token
-        );
+        .set("Authorization", "Bearer " + admin.token);
       expect(response.statusCode).toBe(202);
       expect(response.body.accounts.length).toBeGreaterThanOrEqual(1);
     });
@@ -256,27 +232,21 @@ describe(`Test endpoint ${endpoint}`, () => {
     it("Return 403 when user makes the request", async () => {
       const response = await request
         .get(`${endpoint}/`)
-        .set(
-          "Authorization",
-          "Bearer " + user.token
-        );
+        .set("Authorization", "Bearer " + user.token);
       expect(response.statusCode).toBe(403);
-    })
+    });
   });
 
   describe(`Test POST: ${endpoint}/:id`, async () => {
     it("Return 202 when admin promotes a user", async () => {
       const response = await request
         .post(`${endpoint}/${user.id}`)
-        .set(
-          "Authorization",
-          "Bearer " + admin.token
-        );
+        .set("Authorization", "Bearer " + admin.token);
       expect(response.statusCode).toBe(202);
       const promoted = await prisma.account.findFirst({
         where: {
-          id: user.id
-        }
+          id: user.id,
+        },
       });
       expect(promoted.role).toEqual(Role.Admin);
     });
@@ -284,21 +254,15 @@ describe(`Test endpoint ${endpoint}`, () => {
     it("Return 400 when user id does not exist", async () => {
       const response = await request
         .post(`${endpoint}/${user.id * 2}`)
-        .set(
-          "Authorization",
-          "Bearer " + admin.token
-        );
+        .set("Authorization", "Bearer " + admin.token);
       expect(response.statusCode).toBe(400);
     });
 
     it("Return 403 when providing a user token", async () => {
       const response = await request
         .post(`${endpoint}/${user.id}`)
-        .set(
-          "Authorization",
-          "Bearer " + user.token
-        );
+        .set("Authorization", "Bearer " + user.token);
       expect(response.statusCode).toBe(403);
     });
   });
-})
+});
