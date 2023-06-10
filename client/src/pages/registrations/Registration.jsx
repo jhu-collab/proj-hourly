f044c028add53dda59356e84da1246216f7f9704
Chloe Donelan <chloemdonelan@gmail.com>
2023-06-10 14:12:15 -0400

    added no show button when applicable, working on its actual functionality w/ the hook

--- client/src/pages/registrations/Registration.jsx
+++ client/src/pages/registrations/Registration.jsx
@@ -14,6 +14,7 @@ import useMutationCancelRegistration from "../../hooks/useMutationCancelRegistra
 import useStoreLayout from "../../hooks/useStoreLayout";
 import { decodeToken } from "react-jwt";
 import useStoreToken from "../../hooks/useStoreToken";
+import useMutationChangeNoShowStatus from "../../hooks/useMutationChangeNoShowStatus";
 
 /**
  * Represents a single Registration card.
@@ -24,13 +25,18 @@ import useStoreToken from "../../hooks/useStoreToken";
  * @returns a single Registration component.
  */
 function Registration({ registration, type }) {
-  const { mutate, isLoading: isLoadingMutate } = useMutationCancelRegistration(
+  // const { mutate, isLoading: isLoadingMutate } = useMutationCancelRegistration(
+  //   registration.id || -1
+  // );
+
+  const { mutate, isLoading: isLoadingMutate } = useMutationChangeNoShowStatus(
     registration.id || -1
   );
 
   const courseType = useStoreLayout((state) => state.courseType);
   const token = useStoreToken((state) => state.token);
   let isHost = false;
+  const isNoShow = registration.isNoShow;
 
   const { id } = decodeToken(token);
 
@@ -38,8 +44,14 @@ function Registration({ registration, type }) {
     isHost = true;
   }
 
-  const onClick = () => {
-    confirmDialog("Do you really want to cancel this registration?", () =>
+  const onCancelClick = () => {
+    // confirmDialog("Do you really want to cancel this registration?", () =>
+    //   /mutate()
+    // );
+  };
+
+  const onNoShowClick = () => {
+    confirmDialog("Do you really want to mark this student as a no-show?", () =>
       mutate()
     );
   };
@@ -56,7 +68,7 @@ function Registration({ registration, type }) {
         >
           {/* Date and Time */}
           <Stack direction="row" spacing={5}>
-            <Typography fontWeight={600}>
+            <Typography fontWeight={600} color={isNoShow ? "error.main" : "text.primary"}>
               {DateTime.fromISO(
                 registration.date.substring(0, 10) +
                   registration.startTime.substring(10)
@@ -84,7 +96,7 @@ function Registration({ registration, type }) {
               </Typography>
             )}
             {(courseType === "Instructor" || courseType === "Staff") && (
-              <Typography>
+              <Typography color={isNoShow ? "error.main" : "text.primary"}>
                 Student:{" "}
                 <strong>
                   {registration.account.firstName}{" "}
@@ -126,6 +138,20 @@ function Registration({ registration, type }) {
               <Typography marginBottom={4}>{registration.question}</Typography>
             </>
           )}
+          {type === 2 && 
+            (isHost || courseType === "Instructor") && (
+              <>
+                <Button
+                  variant="contained"
+                  size="large"
+                  fullWidth
+                  onClick={onNoShowClick}
+                >
+                  Mark as No-Show
+                </Button>
+                <ConfirmPopup />
+              </>
+            )}
           {type === 0 &&
             (isHost ||
               courseType === "Student" ||
@@ -135,7 +161,7 @@ function Registration({ registration, type }) {
                   variant="contained"
                   size="large"
                   fullWidth
-                  onClick={onClick}
+                  onClick={onCancelClick}
                 >
                   Cancel
                 </Button>
