7f6841ed24a112dab8d8f5fdaa79bdeae12fa39f
Thomas Li <89786883+tommyli03@users.noreply.github.com>
2023-06-10 18:38:45 -0700

    added edit location button to mobile event popup

--- client/src/pages/calendar/event-details/MobileEventPopup.jsx
+++ client/src/pages/calendar/event-details/MobileEventPopup.jsx
@@ -10,6 +10,7 @@ import EditAction from "./EditAction";
 import EventDetails from "./EventDetails";
 import StudentDetails from "./StudentDetails";
 import NiceModal, { useModal } from "@ebay/nice-modal-react";
+import EditLocation from "./EditLocation";
 import useStoreLayout from "../../../hooks/useStoreLayout";
 
 /**
@@ -35,6 +36,9 @@ const MobileEventPopup = NiceModal.create(() => {
               <IconButton sx={{ fontSize: "20px" }} onClick={modal.hide}>
                 <CloseOutlined />
               </IconButton>
+              {(courseType === "Staff" || courseType === "Instructor") && (
+                <EditLocation />
+              )}
               {(courseType === "Staff" || courseType === "Instructor") && (
                 <EditAction />
               )}
