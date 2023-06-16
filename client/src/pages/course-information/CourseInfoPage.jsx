0da2b4d35241ec583d65b65b83c6c887c8b7c7e0
Chloe Donelan <chloemdonelan@gmail.com>
2023-06-16 16:11:39 -0400

    added form for instructors to add/update course lecture events

--- client/src/pages/course-information/CourseInfoPage.jsx
+++ client/src/pages/course-information/CourseInfoPage.jsx
@@ -4,33 +4,41 @@ import RemoveCourseAction from "./RemoveCourseAction";
 import Stack from "@mui/material/Stack";
 import useStoreCourse from "../../hooks/useStoreCourse";
 import useStoreLayout from "../../hooks/useStoreLayout";
+import LectureEventForm from "./LectureEventForm";
 
 function CourseInfoPage() {
   const course = useStoreCourse((state) => state.course);
   const courseType = useStoreLayout((state) => state.courseType);
   return (
-    <MainCard title="Course Information" sx={{ padding: 0 }} content={false}>
-      <Stack padding="16px" spacing={1}>
-        <Typography variant="h5" fontWeight={400}>
-          Course Name: <strong>{course.title}</strong>
-        </Typography>
-        <Typography variant="h5" fontWeight={400}>
-          Course Number: <strong>{course.courseNumber}</strong>
-        </Typography>
-        <Typography variant="h5" fontWeight={400}>
-          Semester: <strong>{course.semester}</strong>
-        </Typography>
-        <Typography variant="h5" fontWeight={400}>
-          Year: <strong>{course.calendarYear}</strong>
-        </Typography>
-        {(courseType === "Staff" || courseType === "Instructor") && (
+    <>
+      <MainCard title="Course Information" sx={{ padding: 0 }} content={false}>
+        <Stack padding="16px" spacing={1}>
           <Typography variant="h5" fontWeight={400}>
-            Code: <strong>{course.code}</strong>
+            Course Name: <strong>{course.title}</strong>
           </Typography>
-        )}
-      </Stack>
-      {courseType === "Student" && <RemoveCourseAction courseId={course.id} />}
-    </MainCard>
+          <Typography variant="h5" fontWeight={400}>
+            Course Number: <strong>{course.courseNumber}</strong>
+          </Typography>
+          <Typography variant="h5" fontWeight={400}>
+            Semester: <strong>{course.semester}</strong>
+          </Typography>
+          <Typography variant="h5" fontWeight={400}>
+            Year: <strong>{course.calendarYear}</strong>
+          </Typography>
+          {(courseType === "Staff" || courseType === "Instructor") && (
+            <Typography variant="h5" fontWeight={400}>
+              Code: <strong>{course.code}</strong>
+            </Typography>
+          )}
+        </Stack>
+        {courseType === "Student" && <RemoveCourseAction courseId={course.id} />}
+      </MainCard>
+      {courseType === "Instructor" && 
+        <MainCard title="Course Calendar Event Information" sx={{ padding: 0 }} content={true}>
+          <LectureEventForm />
+        </MainCard>
+      }
+    </>
   );
 }
 
