1a33e7095dab66b88279801a63a3e6e6da8b8ea6
Chloe Donelan <chloemdonelan@gmail.com>
2023-06-17 16:00:59 -0400

    course topic object for agenda page (mostly copied stuff from topic for now)

---
+++ client/src/pages/agenda/CourseTopic.jsx
@@ -0,0 +1,128 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import AnimateButton from "../../components/AnimateButton";
+import ConfirmPopup, { confirmDialog } from "../../components/ConfirmPopup";
+import Form from "../../components/form-ui/Form";
+import FormInputText from "../../components/form-ui/FormInputText";
+import MainCard from "../../components/MainCard";
+import useMutationDeleteTopic from "../../hooks/useMutationDeleteTopic";
+import useMutationEditTopic from "../../hooks/useMutationEditTopic";
+import useStoreCourse from "../../hooks/useStoreCourse";
+import useStoreLayout from "../../hooks/useStoreLayout";
+import { topicSchema } from "../../utils/validators";
+
+/**
+ * Represents a single CourseTopic card.
+ * @param {*} courseTopic topic object
+ * @returns a single CourseTopic component.
+ */
+function CourseTopic({ courseTopic }) {
+  const [edit, setEdit] = useState(false);
+
+  const { mutate } = useMutationEditTopic();
+  const { mutate: mutateDelete } = useMutationDeleteTopic();
+
+  const course = useStoreCourse((state) => state.course);
+  const courseType = useStoreLayout((state) => state.courseType);
+
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      name: courseTopic.value,
+    },
+    resolver: yupResolver(topicSchema),
+  });
+
+  const onSubmit = (data) => {
+    mutate({
+      courseId: course.id,
+      topicId: courseTopic.id,
+      value: data.name,
+    });
+    setEdit(false);
+  };
+
+  const handleOnClickCancelBtn = (e) => {
+    e.preventDefault();
+    setEdit(false);
+  };
+
+  const handleOnClickEditBtn = (e) => {
+    e.preventDefault();
+    setEdit(true);
+  };
+
+  return (
+    <>
+      <MainCard sx={{ padding: 2 }} content={false}>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Stack
+            direction={{ xs: "column", sm: "row" }}
+            justifyContent="space-between"
+            alignItems="center"
+            spacing={2}
+          >
+            {edit && courseType === "Instructor" ? (
+              <FormInputText
+                name="name"
+                control={control}
+                sx={{ width: 230 }}
+              />
+            ) : (
+              <Typography variant="h5">{topic.value}</Typography>
+            )}
+            {edit && courseType === "Instructor" && (
+              <Stack direction="row" spacing={1}>
+                <AnimateButton>
+                  <Button variant="contained" type="submit">
+                    Submit
+                  </Button>
+                </AnimateButton>
+                <AnimateButton>
+                  <Button
+                    variant="contained"
+                    color="error"
+                    onClick={handleOnClickCancelBtn}
+                  >
+                    Cancel
+                  </Button>
+                </AnimateButton>
+              </Stack>
+            )}
+            {!edit && courseType === "Instructor" && (
+              <Stack direction="row" spacing={1}>
+                <AnimateButton>
+                  <Button variant="contained" onClick={handleOnClickEditBtn}>
+                    Edit
+                  </Button>
+                </AnimateButton>
+                <AnimateButton>
+                  <Button
+                    variant="contained"
+                    color="error"
+                    onClick={() => {
+                      confirmDialog(
+                        `Do you really want to delete the "${topic.value}" topic?`,
+                        () => {
+                          mutateDelete(topic.id);
+                        }
+                      );
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </AnimateButton>
+              </Stack>
+            )}
+          </Stack>
+        </Form>
+      </MainCard>
+      <ConfirmPopup />
+    </>
+  );
+}
+
+export default CourseTopic;
