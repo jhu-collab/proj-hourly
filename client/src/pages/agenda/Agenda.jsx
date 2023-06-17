1d138acabd81cdf976c432455bdbe086534a6411
Chloe Donelan <chloemdonelan@gmail.com>
2023-06-17 15:58:17 -0400

    new agenda page; empty (besides header) for now

---
+++ client/src/pages/agenda/Agenda.jsx
@@ -0,0 +1,11 @@
+import Typography from "@mui/material/Typography";
+
+function Agenda() {
+  return (
+    <Typography variant="h4" sx={{ marginBottom: 2.25 }}>
+        Agenda
+      </Typography>
+  );
+}
+
+export default Agenda;
