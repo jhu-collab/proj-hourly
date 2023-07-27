d7301e6f4b9c414b3a7e58f7b000c53d6ecbca4c
Chloe Donelan <chloemdonelan@gmail.com>
2023-07-27 19:05:44 -0400

    added action code

---
+++ client/src/pages/calendar/event-details/CourseEventEditAction.jsx
@@ -0,0 +1,23 @@
+import EditOutlined from "@ant-design/icons/EditOutlined";
+import IconButton from "@mui/material/IconButton";
+import NiceModal from "@ebay/nice-modal-react";
+
+/**
+ * Represents the Edit IconButton on the CourseEventPopover component
+ * and the associated ConfirmPopup component.
+ * @returns Course event edit action button and popup.
+ */
+function CourseEventEditAction() {
+  return (
+    <>
+      <IconButton
+        sx={{ fontSize: "20px" }}
+        onClick={() => NiceModal.show("upsert-event", { type: "courseEdit" })}
+      >
+        <EditOutlined />
+      </IconButton>
+    </>
+  );
+}
+
+export default CourseEventEditAction;
