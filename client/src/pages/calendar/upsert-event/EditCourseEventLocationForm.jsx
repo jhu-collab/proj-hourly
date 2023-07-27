3078e31bcd93a804d96c5a517f8f9407b2939586
Chloe Donelan <chloemdonelan@gmail.com>
2023-07-27 18:15:04 -0400

    added skeleton code

---
+++ client/src/pages/calendar/upsert-event/EditCourseEventLocationForm.jsx
@@ -0,0 +1,11 @@
+/**
+ * Component that represents the form that is used to edit a course event's location.
+ * @returns A component representing the Edit Course Event Location form.
+ */
+function EditCourseEventLocationForm() {
+  return (
+    <></>
+  );
+}
+
+export default EditCourseEventLocationForm;
