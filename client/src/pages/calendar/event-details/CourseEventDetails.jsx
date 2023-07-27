9fb53a6b92472c8e0903d898ddf69bebeb6197cc
Chloe Donelan <chloemdonelan@gmail.com>
2023-07-27 17:41:33 -0400

    Added file and skeleton code (some minor updates to come later)

---
+++ client/src/pages/calendar/event-details/CourseEventDetails.jsx
@@ -0,0 +1,37 @@
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import { DateTime } from "luxon";
+import useStoreEvent from "../../../hooks/useStoreEvent";
+
+/**
+ * Child component that displays course calendar event details.
+ * @returns event-specific information
+ */
+function CourseEventDetails() {
+  const title = useStoreEvent((state) => state.title);
+  const start = useStoreEvent((state) => state.start);
+  const location = useStoreEvent((state) => state.location);
+
+  const date = start.toDateString();
+  
+  return (
+    <Stack direction="column" spacing={1}>
+      {/*<Typography variant="h4">{title}</Typography>*/}
+      <Typography variant="h4">Agenda Description Goes Here</Typography>
+      <Typography>
+        <strong>Date: </strong>
+        {date}
+      </Typography>
+      <Typography>
+        <strong>Location: </strong>
+        {location}
+      </Typography>
+      <Typography>
+        <strong>Additional Resources: </strong>
+        {location}
+      </Typography>
+    </Stack>
+  );
+}
+
+export default CourseEventDetails;
