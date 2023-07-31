be81e3e91d41b4bd735b59eebfd7ddf2d3630358
Chloe Donelan <chloemdonelan@gmail.com>
2023-07-31 18:17:06 -0400

    deleted due to issue

--- server/prisma/migrations/20230731194713_agenda_name_change/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `agendaDescrip` on the `CalendarEvent` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "CalendarEvent" DROP COLUMN "agendaDescrip",
-ADD COLUMN     "title" TEXT NOT NULL DEFAULT '';
