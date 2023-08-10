f15a54f3db0302d1a37ed4eef7592b2309ba359d
Chloe Donelan <chloemdonelan@gmail.com>
2023-08-10 16:40:40 -0400

    cleaned up commented stuff

--- client/src/pages/calendar/event-details/CourseEventDeleteAction.jsx
+++ client/src/pages/calendar/event-details/CourseEventDeleteAction.jsx
@@ -3,7 +3,7 @@ import IconButton from "@mui/material/IconButton";
 import ConfirmPopup, { confirmDialog } from "../../../components/ConfirmPopup";
 import Loader from "../../../components/Loader";
 import useStoreEvent from "../../../hooks/useStoreEvent";
-import useMutationCancelCourseCalendarEvent from "../../../hooks/useMutationCancelCourseCalendarEvent";
+import useMutationDeleteCourseCalendarEvent from "../../../hooks/useMutationDeleteCourseCalendarEvent";
 import useStoreCourse from "../../../hooks/useStoreCourse";
 import { useState } from "react";
 import { DateTime } from "luxon";
@@ -19,10 +19,9 @@ function CourseEventDeleteAction() {
   const date = useStoreEvent((state) => state.start);
   const recurring = useStoreEvent((state) => state.recurring);
   const [deleteType, setDeleteType] = useState("this");
-  //const deleteType = "this";
 
   const { mutate, isLoading } =
-    useMutationCancelCourseCalendarEvent(deleteType); // TODO: CHANGE THIS
+    useMutationDeleteCourseCalendarEvent(deleteType);
 
   return (
     <>
@@ -30,7 +29,7 @@ function CourseEventDeleteAction() {
         sx={{ fontSize: "20px" }}
         onClick={() => {
           confirmDialog(
-            "Do you really want to cancel this lecture event?",
+            "Do you really want to delete this lecture event?",
             () =>
               deleteType === "this"
                 ? mutate({
@@ -41,32 +40,11 @@ function CourseEventDeleteAction() {
                   })
                 : mutate({ courseId: course.id })
           );
-          console.log("clicked");
         }}
       >
         <DeleteOutlined />
       </IconButton>
-      <ConfirmPopup
-        /*{...(recurring && { header: "Delete recurring lecture event" })}*/
-      >
-        {/*recurring && (
-          <RadioGroup
-            value={deleteType}
-            onChange={(event) => setDeleteType(event.target.value)}
-          >
-            <FormControlLabel
-              value="this"
-              control={<Radio />}
-              label="This lecture event"
-            />
-            <FormControlLabel
-              value="all"
-              control={<Radio />}
-              label="All lecture events"
-            />
-          </RadioGroup>
-        )*/}
-      </ConfirmPopup>
+      <ConfirmPopup />
       {isLoading && <Loader />}
     </>
   );
