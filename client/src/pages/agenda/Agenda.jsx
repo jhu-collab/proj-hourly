efd74009e0a1da1ccdc232698cac1efb44d514ff
Chloe Donelan <chloemdonelan@gmail.com>
2023-08-11 21:33:58 -0400

    changed to integrate with agenda topics for course events

--- client/src/pages/agenda/Agenda.jsx
+++ client/src/pages/agenda/Agenda.jsx
@@ -1,15 +1,13 @@
-import Fab from "@mui/material/Fab";
-import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 import Alert from "@mui/material/Alert";
-import Grid from "@mui/material/Grid";
-import NiceModal from "@ebay/nice-modal-react";
-import Topic from "../topics/Topic";
 import Typography from "@mui/material/Typography";
-import useQueryTopics from "../../hooks/useQueryTopics";
 import useStoreLayout from "../../hooks/useStoreLayout";
+import useQueryCourseEvents from "../../hooks/useQueryCourseEvents";
+import { Grid } from "@mui/material";
+import CourseTopic from "./CourseTopic";
+import { DateTime } from "luxon";
 
 function Agenda() {
-  const { isLoading, error, data } = useQueryTopics(); // TODO: CHANGE THIS LATER FOR LECTURE TOPICS
+  const { isLoading, error, data } = useQueryCourseEvents();
   const courseType = useStoreLayout((state) => state.courseType);
 
   if (isLoading) {
@@ -20,35 +18,28 @@ function Agenda() {
     return <Alert severity="error">Unable to retrieve lecture topics</Alert>;
   }
 
-  // TODO: CHANGE LATER FOR LECTURE TOPICS
   return (
     <>
       <Typography variant="h4" sx={{ marginBottom: 2.25 }}>
         Agenda
       </Typography>
-      {data.length !== 0 && (
-        <Grid container spacing={2}>
-          {data.map((topic) => {
-            return (
-              <Grid item xs={12} key={topic.id}>
-                <Topic topic={topic} />
-              </Grid>
-            );
-          })}
-        </Grid>
-      )}
       {courseType === "Instructor" && (
-        <Fab
-          color="primary"
-          onClick={() => NiceModal.show("create-topic")}
-          sx={{
-            position: "fixed",
-            bottom: (theme) => theme.spacing(3),
-            right: (theme) => theme.spacing(3),
-          }}
-        >
-          <SpeedDialIcon />
-        </Fab>
+        <>
+          <Grid container spacing={2}>
+            {data.calendarEvents.map((calendarEvent) => {
+              return (
+                <Grid item xs={12} key={calendarEvent.date}>
+                  <Grid item xs={12} key={calendarEvent.date}>
+                    <CourseTopic
+                      topic={calendarEvent.title}
+                      date={calendarEvent.date}
+                    />
+                  </Grid>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </>
       )}
     </>
   );
