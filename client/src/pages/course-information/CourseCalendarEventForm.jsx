07edfe6c7666f77686d7a77d55cf6fc51c468a58
Chloe Donelan <chloemdonelan@gmail.com>
2023-08-12 19:21:51 -0400

    removed console log

--- client/src/pages/course-information/CourseCalendarEventForm.jsx
+++ client/src/pages/course-information/CourseCalendarEventForm.jsx
@@ -126,7 +126,6 @@ function CourseCalendarEventForm() {
   };
 
   const doEventsExist = () => {
-    console.log(courseEventsData);
     if (
       Array.isArray(courseEventsData?.calendarEvents) &&
       courseEventsData &&
