5b1df7017205ea52f922f60cf208be5b8f4844a9
Chloe Donelan <chloemdonelan@gmail.com>
2023-08-12 17:28:24 -0400

    cleaned up & formatted

--- client/src/hooks/useMutationCreateCourseCalendarEvent.jsx
+++ client/src/hooks/useMutationCreateCourseCalendarEvent.jsx
@@ -4,22 +4,14 @@ import { errorToast } from "../utils/toasts";
 import { getConfig } from "./helper";
 import { toast } from "react-toastify";
 import { BASE_URL } from "../services/common";
-import useTheme from "@mui/material/styles/useTheme";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import useStoreToken from "./useStoreToken";
-import useStoreLayout from "./useStoreLayout";
 import Debug from "debug";
 
-const debug = new Debug(`hourly:hooks:useMutationCreateCourseCalendar.jsx`);
+const debug = new Debug(`hourly:hooks:useMutationCreateCourseCalendarEvent.jsx`);
 
 function useMutationCreateCourseCalendarEvent() {
   const { token } = useStoreToken();
   const queryClient = useQueryClient();
-
-  const theme = useTheme();
-  const matchUpSm = useMediaQuery(theme.breakpoints.up("sm"));
-
-  const setAnchorEl = useStoreLayout((state) => state.setEventAnchorEl);
 
   const createCourseCalendarEvent = async (courseEvent) => {
     console.log(courseEvent);
@@ -34,25 +26,13 @@ function useMutationCreateCourseCalendarEvent() {
     }
   };
 
-  // const createRecurringCourseCalendarEvent = async (courseEvent) => {
-  //   try {
-  //     debug("Sending recurring course calendar event to be created to the backend...");
-  //     const endpoint = `${BASE_URL}/api/courseCalendar/createRecurringEvent`;
-  //     const res = await axios.post(endpoint, courseEvent, getConfig(token));
-  //     debug("Successful! Returning result data...");
-  //     return res.data;
-  //   } catch (err) {
-  //     throw err;
-  //   }
-  // }
-
   const mutation = useMutation(createCourseCalendarEvent, {
     onSuccess: (data) => {
       const courseEvent = data;
 
       queryClient.invalidateQueries(["courseEvents"]);
 
-      toast.success("Created recurring course calendar event!");
+      toast.success("Created course calendar events!");
       console.log(courseEvent);
     },
     onError: (error) => {
