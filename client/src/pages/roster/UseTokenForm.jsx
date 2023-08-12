12f234ab953e8e30262e5c7f116151e4b4b8564a
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-08-12 19:53:42 -0400

    added undo token

--- client/src/pages/roster/UseTokenForm.jsx
+++ client/src/pages/roster/UseTokenForm.jsx
@@ -2,15 +2,15 @@ import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Radio from "@mui/material/Radio";
 import FormInputDropdown from "../../components/form-ui/FormInputDropdown";
 import Form from "../../components/form-ui/Form";
 import { useForm } from "react-hook-form";
 import useQueryRemainingTokensPerStudent from "../../hooks/useQueryGetRemainingTokensPerStudent";
 import useMutationUseToken from "../../hooks/useMutationUseToken";
 import useStoreCourse from "../../hooks/useStoreCourse";
+import FormCheckbox from "../../components/form-ui/FormCheckbox";
+import FormInputText from "../../components/form-ui/FormInputText";
+import useMutationUndoUseToken from "../../hooks/useMutationUndoUseToken";
 
 const getTokenOptions = (tokens) => {
   const tokenArr = [];
@@ -35,9 +35,12 @@ function UseTokenForm(props) {
 
   const { mutate } = useMutationUseToken(params.id, course.id);
 
+  const { mutate: undoMutate } = useMutationUndoUseToken(params.id, course.id);
+
   const { control, handleSubmit, watch } = useForm({
     defaultValues: {
       token: "",
+      undoToken: false,
     },
   });
 
@@ -52,11 +55,16 @@ function UseTokenForm(props) {
         selectedToken = token.CourseToken.id;
       }
     });
-    mutate(selectedToken);
+    if (!e.undoToken) {
+      mutate(selectedToken);
+    } else {
+      undoMutate({ token: selectedToken, date: e.date });
+    }
   };
 
   // tokens with students count
   const token = watch("token");
+  const undo = watch("undoToken");
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
@@ -70,6 +78,20 @@ function UseTokenForm(props) {
           label="Token Type"
           options={tokens}
         />
+        <FormCheckbox
+          name="undoToken"
+          control={control}
+          label="Undo Student Token Usage?"
+        />
+        {undo && (
+          <FormInputText
+            name="date"
+            control={control}
+            label="Date"
+            type="date"
+            InputLabelProps={{ shrink: true }}
+          />
+        )}
         <Button color="secondary" variant="contained" type="submit">
           Submit
         </Button>
