4fc0350e12523f98b46ecc232114ff7c3817e8f4
Chloe Donelan <chloemdonelan@gmail.com>
2023-08-15 12:18:08 -0400

    fixed minor route mixup

--- client/src/hooks/useMutationCreateCourseCalendarEvent.jsx
+++ client/src/hooks/useMutationCreateCourseCalendarEvent.jsx
@@ -6,18 +6,31 @@ import { toast } from "react-toastify";
 import { BASE_URL } from "../services/common";
 import useStoreToken from "./useStoreToken";
 import Debug from "debug";
+import NiceModal from "@ebay/nice-modal-react";
 
 const debug = new Debug(
   `hourly:hooks:useMutationCreateCourseCalendarEvent.jsx`
 );
 
-function useMutationCreateCourseCalendarEvent() {
+function useMutationCreateCourseCalendarEvent(createType) {
   const { token } = useStoreToken();
   const queryClient = useQueryClient();
 
   const createCourseCalendarEvent = async (courseEvent) => {
     try {
       debug("Sending course calendar event to be created to the backend...");
+      const endpoint = `${BASE_URL}/api/calendarEvent/createEvent`;
+      const res = await axios.post(endpoint, courseEvent, getConfig(token));
+      debug("Successful! Returning result data...");
+      return res.data;
+    } catch (err) {
+      throw err;
+    }
+  };
+  
+  const createCourseCalendarEvents = async (courseEvent) => {
+    try {
+      debug("Sending course calendar events to be created to the backend...");
       const endpoint = `${BASE_URL}/api/calendarEvent/create`;
       const res = await axios.post(endpoint, courseEvent, getConfig(token));
       debug("Successful! Returning result data...");
@@ -27,11 +40,13 @@ function useMutationCreateCourseCalendarEvent() {
     }
   };
 
-  const mutation = useMutation(createCourseCalendarEvent, {
+  const mutation = useMutation(
+    createType === "many" ? createCourseCalendarEvents : createCourseCalendarEvent, {
     onSuccess: (data) => {
       const courseEvent = data;
 
       queryClient.invalidateQueries(["courseEvents"]);
+      NiceModal.hide("create-course-calendar-event");
 
       toast.success("Successfully created course calendar events!");
     },
