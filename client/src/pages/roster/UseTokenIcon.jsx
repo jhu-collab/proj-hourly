97d0a68184598f513a71a32274409ae955ec9fa1
jstelnik <jstelni1@jhu.edu>
2023-10-01 15:42:53 -0400

    course token bug fix

--- client/src/pages/roster/UseTokenIcon.jsx
+++ client/src/pages/roster/UseTokenIcon.jsx
@@ -2,10 +2,13 @@ import NiceModal from "@ebay/nice-modal-react";
 import { GridActionsCellItem } from "@mui/x-data-grid";
 import DollarOutlined from "@ant-design/icons/DollarOutlined";
 import useStoreLayout from "../../hooks/useStoreLayout";
+import useStoreCourse from "../../hooks/useStoreCourse";
 
 function UseTokenIcon(props) {
   const { params, isStaff, index } = props;
   const courseType = useStoreLayout((state) => state.courseType);
+  const course = useStoreCourse((state) => state.course);
+  console.log(course)
 
   return (
     <>
@@ -17,7 +20,7 @@ function UseTokenIcon(props) {
             isStaff: isStaff,
           });
         }}
-        disabled={courseType === "Student" || index !== 0}
+        disabled={courseType === "Student" || !course.usesTokens || index !== 0}
         label="Change Role"
         size="large"
       />
