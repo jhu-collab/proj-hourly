284afc5dcec7d76b727313cb715da580d0fccd72
jstelnik <jstelni1@jhu.edu>
2023-10-08 15:34:16 -0400

    error changing token to object --> fixed

--- client/src/pages/roster/UseTokenForm.jsx
+++ client/src/pages/roster/UseTokenForm.jsx
@@ -61,7 +61,7 @@ function UseTokenForm(props) {
       }
     });
     if (!e.undoToken) {
-      mutate({ token: selectedToken });
+      mutate({ selectedToken });
     } else {
       undoMutate({ token: selectedToken, date: e.date });
     }
