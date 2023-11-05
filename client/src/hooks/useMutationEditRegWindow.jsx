e8b0563ba6a565ba30070dc5f836619fd2401f9b
ChrisWilhelm <chriswilhelm2019@gmail.com>
2023-11-05 15:03:31 -0500

    fixed updating course default values

--- client/src/hooks/useMutationEditRegWindow.jsx
+++ client/src/hooks/useMutationEditRegWindow.jsx
@@ -11,65 +11,49 @@ import { toast } from "react-toastify";
 const debug = new Debug(`hourly:hooks:useMutationEditCourseCalendarEvent.jsx`);
 
 function useMutationEditRegWindow(newStart, newEnd) {
-    const { token } = useStoreToken();
-    const queryClient = useQueryClient();
+  const { token } = useStoreToken();
+  const queryClient = useQueryClient();
 
-    const course = useStoreCourse((state) => state.course);
+  const course = useStoreCourse((state) => state.course);
+  const setCourse = useStoreCourse((state) => state.setCourse);
 
+  const editRegWindow = async (updatedRegWindow) => {
+    try {
+      debug("Sending updated registration window to the backend...");
+      const endpoint = `${BASE_URL}/api/course/${course.id}/registrationConstraints`;
+      const res = await axios.post(
+        endpoint,
+        updatedRegWindow,
+        getConfig(token)
+      );
+      debug("Successful! Returning result data...");
+      console.log(res.data);
+      return res.data;
+    } catch (err) {
+      throw err;
+    }
+  };
 
+  const mutation = useMutation(editRegWindow, {
+    onSuccess: (data) => {
+      const updatedRegWindow = data.course;
+      queryClient.invalidateQueries(["courses"]);
+      queryClient.invalidateQueries(["courseEvents"]);
+      queryClient.invalidateQueries(["officeHour"]);
+      setCourse(updatedRegWindow);
+      toast.success(
+        `Successfully changed ${updatedRegWindow.title}'s registration constraint for start: ${updatedRegWindow.startRegConstraint} and end: ${updatedRegWindow.endRegConstraint}`
+      );
+    },
+    onError: (error) => {
+      debug({ error });
+      errorToast(error);
+    },
+  });
 
-    const editRegWindow = async (updatedRegWindow) => {
-        try {
-            debug("Sending updated registration window to the backend...");
-            const endpoint = `${BASE_URL}/api/course/${course.id}/registrationConstraints`;
-            const res = await axios.post(endpoint, updatedRegWindow, getConfig(token));
-            debug("Successful! Returning result data...");
-            console.log(res.data)
-            return res.data;
-
-        } catch (err) {
-            throw err;
-        }
-    };
-
-    const mutation = useMutation(editRegWindow, {
-        onSuccess: (data) => {
-            const updatedRegWindow = data.course;
-            queryClient.invalidateQueries(["courses"]);
-            queryClient.invalidateQueries(["courseEvents"]);
-            queryClient.invalidateQueries(["officeHour"]);
-            queryClient.invalidateQueries(["registrationStatus"]);
-            queryClient.invalidateQueries(["allRegistrations"]);
-            queryClient.invalidateQueries(["courseUsers", course.id]);
-            queryClient.invalidateQueries(["myRole"]);
-            queryClient.invalidateQueries(["officeHours"]);
-            queryClient.invalidateQueries(["registrationTypes"]);
-            queryClient.invalidateQueries(["tokens"]);
-            queryClient.invalidateQueries(["tokenCounts"]);
-            queryClient.invalidateQueries(["topics"]);
-            queryClient.invalidateQueries(["topicCounts"]);
-            queryClient.invalidateQueries(["studentRegistrationCounts"]);
-            queryClient.invalidateQueries(["topicCounts"]);
-            queryClient.invalidateQueries(["studentRegistrationCounts"]);
-            queryClient.invalidateQueries(["courseUsers"]);
-            queryClient.invalidateQueries(["remainingTokensPerStudent"]);
-            queryClient.invalidateQueries(["remainingTokens"]);
-
-            toast.success(
-                `Successfully changed ${updatedRegWindow.title}'s registration constraint for start: ${updatedRegWindow.startRegConstraint} and end: ${updatedRegWindow.endRegConstraint}`
-            );
-
-
-        },
-        onError: (error) => {
-            debug({ error });
-            errorToast(error);
-        },
-    });
-
-    return {
-        ...mutation,
-    };
+  return {
+    ...mutation,
+  };
 }
 
 export default useMutationEditRegWindow;
